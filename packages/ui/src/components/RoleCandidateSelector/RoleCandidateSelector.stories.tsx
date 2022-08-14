a74f88aa0416a28496f0bf6a17a7d2adc4d24293
dkh215 <danielkhunter@gmail.com>
2022-08-13 23:04:16 -0400

    feat: Add RoleCandidateSelector component
    Add react-icons package to web workspace
    
    Add icons for left and right buttons
    
    Lint files
    
    Add disabled states
    
    Lint file

---
+++ packages/ui/src/components/RoleCandidateSelector/RoleCandidateSelector.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { RoleCandidateSelector } from "./RoleCandidateSelector";
+
+export default {
+  title: "Components/RoleCandidateSelector",
+  component: RoleCandidateSelector,
+  argTypes: {},
+} as ComponentMeta<typeof RoleCandidateSelector>;
+
+const roles = [
+  { _id: 1, name: "Scrum Master" },
+  { _id: 2, name: "Frontend" },
+  { _id: 3, name: "Backend" },
+  { _id: 4, name: "Product Manager" },
+];
+
+const Template: ComponentStory<typeof RoleCandidateSelector> = (args) => (
+  <RoleCandidateSelector {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  roles: roles,
+
+};
