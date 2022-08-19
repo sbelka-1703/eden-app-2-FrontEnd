d4d712aa546d227275dd07caef1705743e6f4f42
ateet <ateet@propelld.com>
2022-08-19 16:22:23 +0530

    style: badge component get formated

--- packages/ui/src/elements/Badge/Badge.test.tsx
+++ packages/ui/src/elements/Badge/Badge.test.tsx
@@ -4,7 +4,13 @@ import { Badge } from "./";
 
 describe("Badge", () => {
   it("renders without throwing", () => {
-    const { container } = render(<Badge colorRGB="236, 240, 71" text="Design" onClick={() => console.log("")}/>);
+    const { container } = render(
+      <Badge
+        colorRGB="236, 240, 71"
+        text="Design"
+        onClick={() => console.log("")}
+      />
+    );
 
     expect(container).toBeInTheDocument();
   });
