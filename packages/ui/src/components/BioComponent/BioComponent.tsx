978daa63936e58791b3f239f890eef85041cc8b6
ateet <ateet@propelld.com>
2022-08-19 15:58:04 +0530

    feat: BIO Component

---
+++ packages/ui/src/components/BioComponent/BioComponent.tsx
@@ -0,0 +1,43 @@
+import { Avatar } from "../../elements";
+import { FiEdit3 } from "react-icons/fi";
+export interface IBioComponentProps {
+  title?: string;
+  description?: string;
+  onEdit?: () => void;
+  isEditable?: boolean;
+}
+
+export const BioComponent = ({
+  title,
+  description,
+  onEdit,
+  isEditable,
+}: IBioComponentProps) => {
+  return (
+    <div
+      className={`ml-2 w-96 rounded-3xl border-2 bg-white p-1 drop-shadow-2xl`}
+    >
+      <div className="my-4 flex flex-col">
+        <div
+          className={`mx-4 flex flex-row content-center justify-between justify-items-stretch`}
+        >
+          <div>
+            <div className={`text-2xl`}>{title}</div>
+          </div>
+          <div className="mt-1">
+            {isEditable ? (
+              <button className={`w-full text-zinc-400`} onClick={onEdit}>
+                <FiEdit3 size="20px" />
+              </button>
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+        <div className={`mx-6 my-2 text-lg text-slate-700`}>
+          <p className="break-words text-justify">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
