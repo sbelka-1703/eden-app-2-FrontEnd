eb59184ab7fef94f2a72c6c0d51a32b88e6bd1de
yunggenius <yunggenius4@gmail.com>
2022-08-22 15:43:45 +0100

    built out horizontally scrollable carousel component

--- packages/ui/src/components/Endorsements/Endorsements.tsx
+++ packages/ui/src/components/Endorsements/Endorsements.tsx
@@ -2,22 +2,22 @@ import { UserWithDescription } from "../UserWithDescription";
 import "./styles.css"
 
 export interface IEndorsements {
-    title?: string;
-    endorsement?: string;
+  title?: string;
+  endorsement?: string;
 }
 
 export const Endorsements = ({ title, endorsement }: IEndorsements) => {
-    return (
-        <div className="endorsements-box ">
-            <div className="text-center p-3 ">
-                <UserWithDescription avatarSrc="h" />
-                <p className="end-p">{title}</p>
-                <h1>{endorsement}</h1>
-                <div className=" mt-2 buttons flex justify-around">
-                    <button className="border text-white endorsements-button">Remove</button>
-                    <button className="bg-white border endorsements-button">Claim</button>
-                </div>
-            </div>
+  return (
+    <div className="endorsements-box px-5 m-5">
+      <div className="text-center p-3 ">
+        <UserWithDescription avatarSrc="h" />
+        <p className="end-p">{title}</p>
+        <h1>{endorsement}</h1>
+        <div className=" mt-2 buttons flex justify-around">
+          <button className="border text-white endorsements-button">Remove</button>
+          <button className="bg-white border endorsements-button">Claim</button>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- packages/ui/src/components/EndorsmentsCarousel/EndorsementsCarousel.stories.tsx
+++ packages/ui/src/components/EndorsmentsCarousel/EndorsementsCarousel.stories.tsx
@@ -3,16 +3,18 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { EndorsementsCarousel } from "./EndorsmentsCarousel";
 
 export default {
-    title: "Components/EndorsmentsCarousel",
-    component: EndorsementsCarousel,
-    argTypes: {},
+  title: "Components/EndorsmentsCarousel",
+  component: EndorsementsCarousel,
+  argTypes: {},
 } as ComponentMeta<typeof EndorsementsCarousel>;
 
 const Template: ComponentStory<typeof EndorsementsCarousel> = (args) => (
-    <EndorsementsCarousel {...args} />
+  <EndorsementsCarousel {...args} />
 );
 
 
 export const Default = Template.bind({});
 Default.args = {
+  title: 'endorsed for',
+  endorsement: 'HACKING'
 };
