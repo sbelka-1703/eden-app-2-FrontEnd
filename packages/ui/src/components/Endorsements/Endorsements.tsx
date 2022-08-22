41614c67f619a0070f299581d5c460d3c394c4c7
yunggenius <yunggenius4@gmail.com>
2022-08-23 00:34:30 +0100

    lint error fixes

--- packages/ui/src/components/Endorsements/Endorsements.tsx
+++ packages/ui/src/components/Endorsements/Endorsements.tsx
@@ -2,16 +2,15 @@ import { UserWithDescription } from "../UserWithDescription";
 import "./styles.css"
 
 export interface IEndorsements {
-  title?: string;
-  endorsement?: string;
+  endorsement?: any;
 }
 
-export const Endorsements = ({ title, endorsement }: IEndorsements) => {
+export const Endorsements = ({ endorsement, }: IEndorsements) => {
   return (
     <div className="endorsements-box px-5 m-5">
       <div className="text-center p-3 ">
-        <UserWithDescription avatarSrc="h" />
-        <p className="end-p">{title}</p>
+        <UserWithDescription avatarSrc="url" />
+        <p className="end-p">endorsed you for</p>
         <h1>{endorsement}</h1>
         <div className=" mt-2 buttons flex justify-around">
           <button className="border text-white endorsements-button">Remove</button>
