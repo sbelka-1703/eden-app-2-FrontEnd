7811fa7e7a08707d8986390ce5aaad4401d0d868
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2022-08-24 01:30:28 +0530

    fix: testing errors

--- packages/ui/src/components/LoginButton/LoginButton.stories.tsx
+++ packages/ui/src/components/LoginButton/LoginButton.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { SessionProvider } from "next-auth/react";
 
 import { LoginButton } from "./LoginButton";
 
@@ -9,7 +10,30 @@ export default {
 } as ComponentMeta<typeof LoginButton>;
 
 const Template: ComponentStory<typeof LoginButton> = () => {
-  return <LoginButton />;
+  return (
+    <SessionProvider
+      session={{
+        expires: "1",
+        user: {
+          email: "a",
+          name: "Miral",
+          image:
+            "https://pbs.twimg.com/profile_images/1513838045589430277/4Pxad6DL_400x400.jpg",
+        },
+      }}
+    >
+      <LoginButton />
+    </SessionProvider>
+  );
+};
+
+const NotLoggedIn: ComponentStory<typeof LoginButton> = () => {
+  return (
+    <SessionProvider session={null}>
+      <LoginButton />
+    </SessionProvider>
+  );
 };
 
 export const Default = Template.bind({});
+export const NotLogged = NotLoggedIn.bind({});
--- packages/ui/src/components/LoginButton/LoginButton.test.tsx
+++ packages/ui/src/components/LoginButton/LoginButton.test.tsx
@@ -1,10 +1,20 @@
 import { render } from "@testing-library/react";
+import { SessionProvider } from "next-auth/react";
 
 import { LoginButton } from ".";
 
 describe("LoginButton", () => {
   it("renders without throwing", () => {
-    const { container } = render(<LoginButton />);
+    const { container } = render(
+      <SessionProvider
+        session={{
+          expires: "1",
+          user: { email: "a", name: "Miral", image: "c" },
+        }}
+      >
+        <LoginButton />
+      </SessionProvider>
+    );
 
     expect(container).toBeInTheDocument();
   });
