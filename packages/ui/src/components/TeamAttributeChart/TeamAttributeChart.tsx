017ec802c7013b674fa8d3578b5413b2e0a19e1b
subject026 <subject026@protonmail.com>
2022-08-24 20:03:33 +0100

    setup chart

--- packages/ui/src/components/TeamAttributeChart/TeamAttributeChart.tsx
+++ packages/ui/src/components/TeamAttributeChart/TeamAttributeChart.tsx
@@ -1,5 +1,71 @@
-export interface ITeamAttributeChartProps {}
+import { AttributesType } from "@graphql/eden/generated";
+import {
+  Chart as ChartJS,
+  Filler,
+  Legend,
+  LineElement,
+  PointElement,
+  RadialLinearScale,
+  Tooltip,
+} from "chart.js";
+import { Radar } from "react-chartjs-2";
 
-export const TeamAttributeChart = ({}: ITeamAttributeChartProps) => {
-  return <div>chart</div>;
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
+export interface IMember {
+  memberInfo: {
+    discordName: string;
+    attributes: AttributesType;
+  };
+}
+export interface ITeamAttributeChartProps {
+  members: IMember[];
+}
+
+const colors = [
+  "#1abc9c60",
+  "#2ecc7160",
+  "#3498db60",
+  "#9b59b660",
+  "#34495e60",
+  "#f1c40f60",
+  "#e67e2260",
+  "#e74c3c60",
+  "#ecf0f160",
+  "#95a5a660",
+];
+
+export const TeamAttributeChart = ({ members }: ITeamAttributeChartProps) => {
+  const labels = Object.keys(members[0].memberInfo.attributes);
+  const datasets = members.map(({ memberInfo }, i) => {
+    return {
+      label: memberInfo.discordName,
+      data: Object.keys(memberInfo.attributes).map(
+        (key) => memberInfo.attributes[key as keyof AttributesType]
+      ),
+      fill: true,
+      backgroundColor: colors[i],
+      borderColor: colors[i],
+      pointBackgroundColor: colors[i],
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: colors[i],
+    };
+  });
+
+  return (
+    <Radar
+      data={{
+        labels,
+        datasets,
+      }}
+    />
+  );
 };
