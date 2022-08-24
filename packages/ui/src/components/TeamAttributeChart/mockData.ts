96a6129a068a9d2d6e61ce9d819f1bcdd71eafb6
subject026 <subject026@protonmail.com>
2022-08-24 20:03:52 +0100

    setup some initial mock data

---
+++ packages/ui/src/components/TeamAttributeChart/mockData.ts
@@ -0,0 +1,19 @@
+import { IMember } from "./TeamAttributeChart";
+
+export const mockTeamData: IMember[] = [
+  {
+    memberInfo: {
+      discordName: "Satoshi",
+      attributes: {
+        Coordinator: 0,
+        Director: 0,
+        Helper: 0,
+        Inspirer: 0,
+        Motivator: 0,
+        Observer: 0,
+        Reformer: 0,
+        Supporter: 0,
+      },
+    },
+  },
+];
