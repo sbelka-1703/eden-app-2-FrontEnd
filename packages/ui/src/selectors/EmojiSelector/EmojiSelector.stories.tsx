ba52ab7538efb4cd1024a9678d6e8b642fa1c00a
ateet <ateet@propelld.com>
2022-08-26 19:35:18 +0530

    feat: Emoji Selector

--- packages/ui/src/selectors/EmojiSelector/EmojiSelector.stories.tsx
+++ packages/ui/src/selectors/EmojiSelector/EmojiSelector.stories.tsx
@@ -8,26 +8,10 @@ export default {
   argTypes: {},
 } as ComponentMeta<typeof EmojiSelector>;
 
-const skills = [
-  {
-    _id: "1",
-    name: "JavaScript",
-    __typename: "Skill",
-  },
-];
-
-const roles = [
-  { _id: "1", title: "Scrum Master", skills, __typename: "Role" },
-  { _id: "2", title: "Frontend", skills, __typename: "Role" },
-  { _id: "3", title: "Backend", skills, __typename: "Role" },
-  { _id: "4", title: "Product Manager", skills, __typename: "Role" },
-];
 
 const Template: ComponentStory<typeof EmojiSelector> = (args) => (
   <EmojiSelector {...args} />
 );
 
 export const Default = Template.bind({});
-Default.args = {
-  roles,
-};
+Default.args = {};
--- packages/ui/src/selectors/EmojiSelector/EmojiSelector.tsx
+++ packages/ui/src/selectors/EmojiSelector/EmojiSelector.tsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react";
-import { Button } from "../../elements";
+import { useState } from "react";
 import dynamic from "next/dynamic";
-export interface IEmojiSelectorProps {
-  onSelect: (emoji: string) => void;
-}
+export interface IEmojiSelectorProps {}
 
-export const EmojiSelector = ({ onSelect }: IEmojiSelectorProps) => {
+export const EmojiSelector = ({ }: IEmojiSelectorProps) => {
   const [emojiSelect, setEmojiSelect] = useState(false);
   const [showEmojiSelector, setShowEmojiSelector] = useState(false);
-  const [selectedEmoji, setSelectedEmoji] = useState('')
+  const [selectedEmoji, setSelectedEmoji] = useState("@")
   const Picker = dynamic(() => import("emoji-picker-react"), {
     ssr: false,
   });
@@ -16,28 +13,22 @@ export const EmojiSelector = ({ onSelect }: IEmojiSelectorProps) => {
   const onEmojiClick = (event: any, emojiObject: any) => {
     setEmojiSelect(!emojiSelect);
     console.log(emojiObject.emoji);
-    onSelect(emojiObject.emoji);
     setSelectedEmoji(emojiObject.emoji);
     setShowEmojiSelector(false);
   };
 
-  const onShowEmojiSelector = () => {};
+  const onShowEmojiSelector = () => {
+    setShowEmojiSelector(true)
+  };
 
   return (
     <div className="flex flex-row">
       <div>
-        <div className="bg-emerald-800 rounded-full p-8 text-5xl">
-          {selectedEmoji ? selectedEmoji : "?"}
+        <div className="bg-emerald-500 rounded-full p-7 text-5xl cursor-pointer" onClick={()=>onShowEmojiSelector()}>
+          {selectedEmoji ? selectedEmoji : " ? "}
         </div>
       </div>
       <div>
-        <Button
-          onClick={() => setShowEmojiSelector(!showEmojiSelector)}
-          variant="primary"
-          radius="pill"
-        >
-          Select Emoji
-        </Button>
         {showEmojiSelector ? (
           <Picker
             onEmojiClick={onEmojiClick}
