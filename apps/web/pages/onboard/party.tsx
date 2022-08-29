1eea14a464c074a8291b30efbc0b51563ca62ee5
heavensbee001 <heavensbee001@gmail.com>
2022-08-29 14:32:25 +0200

    Add current logged in user

--- apps/web/pages/onboard/party.tsx
+++ apps/web/pages/onboard/party.tsx
@@ -1,10 +1,12 @@
 // import { useQuery } from "@apollo/client";
 // import { FIND_MEMBERS, FIND_SKILLS } from "@graphql/eden";
 import { useQuery } from "@apollo/client";
+import { UserContext } from "@context/eden";
 import { FIND_MEMBERS, FIND_SKILLS } from "@graphql/eden";
 import { Members } from "@graphql/eden/generated";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useContext } from "react";
 import {
   Avatar,
   Badge,
@@ -48,7 +50,8 @@ const OnboardPartyPage: NextPage = () => {
   // ];
   const router = useRouter();
 
-  // const { currentUser } = useContext(UserContext);
+  const { currentUser } = useContext(UserContext);
+
   const { data: dataMembers } = useQuery(FIND_MEMBERS, {
     variables: {
       fields: {
@@ -71,17 +74,23 @@ const OnboardPartyPage: NextPage = () => {
   return (
     <GridLayout>
       <GridItemThree>
-        <Card shadow className="bg-white p-3">
-          <TextHeading3 className="mb-2">Edit Your Profile Card</TextHeading3>
-          <div className="mb-4 flex items-center">
-            <Avatar
-              src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/598.jpg"
-              size="sm"
+        {currentUser && (
+          <Card shadow className="bg-white p-3">
+            <TextHeading3 className="mb-2">Edit Your Profile Card</TextHeading3>
+            <div className="mb-4 flex items-center">
+              {currentUser.discordAvatar && (
+                <Avatar src={currentUser.discordAvatar} size="sm" />
+              )}
+              {currentUser.discordName && (
+                <span className="ml-2">{currentUser?.discordName}</span>
+              )}
+            </div>
+            <SkillSelector
+              showSelected
+              options={dataSkills?.findSkills || []}
             />
-            <span className="ml-2">exwhyzee.eth</span>
-          </div>
-          <SkillSelector showSelected options={dataSkills?.findSkills || []} />
-        </Card>
+          </Card>
+        )}
       </GridItemThree>
       <GridItemNine>
         <Card shadow className="bg-white p-3">
