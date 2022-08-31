1a0ae0b3f992f9e25da6bf7f766b5eae9b72d6d6
subject026 <subject026@protonmail.com>
2022-08-31 09:37:11 +0100

    add more mock data

--- packages/ui/src/components/TeamAttributeChart/mockData.ts
+++ packages/ui/src/components/TeamAttributeChart/mockData.ts
@@ -3,16 +3,76 @@ import { IMember } from "./TeamAttributeChart";
 export const mockTeamData: IMember[] = [
   {
     memberInfo: {
-      discordName: "Satoshi",
+      discordName: "Kwame",
       attributes: {
-        Coordinator: 0,
-        Director: 0,
-        Helper: 0,
-        Inspirer: 0,
-        Motivator: 0,
-        Observer: 0,
-        Reformer: 0,
-        Supporter: 0,
+        Coordinator: 3,
+        Director: 10,
+        Helper: 1,
+        Inspirer: 8,
+        Motivator: 5,
+        Observer: 10,
+        Reformer: 3,
+        Supporter: 6,
+      },
+    },
+  },
+  {
+    memberInfo: {
+      discordName: "Wheeler",
+      attributes: {
+        Coordinator: 8,
+        Director: 4,
+        Helper: 7,
+        Inspirer: 2,
+        Motivator: 4,
+        Observer: 6,
+        Reformer: 1,
+        Supporter: 2,
+      },
+    },
+  },
+  {
+    memberInfo: {
+      discordName: "Linka",
+      attributes: {
+        Coordinator: 2,
+        Director: 4,
+        Helper: 6,
+        Inspirer: 1,
+        Motivator: 5,
+        Observer: 2,
+        Reformer: 2,
+        Supporter: 1,
+      },
+    },
+  },
+  {
+    memberInfo: {
+      discordName: "Gi",
+      attributes: {
+        Coordinator: 4,
+        Director: 3,
+        Helper: 7,
+        Inspirer: 10,
+        Motivator: 3,
+        Observer: 4,
+        Reformer: 1,
+        Supporter: 9,
+      },
+    },
+  },
+  {
+    memberInfo: {
+      discordName: "Ma-Ti",
+      attributes: {
+        Coordinator: 10,
+        Director: 1,
+        Helper: 9,
+        Inspirer: 8,
+        Motivator: 9,
+        Observer: 2,
+        Reformer: 5,
+        Supporter: 4,
       },
     },
   },
