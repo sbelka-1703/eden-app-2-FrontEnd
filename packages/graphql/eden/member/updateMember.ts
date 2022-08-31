743f63090b221c7305a50a1766b439d5d8e4702a
heavensbee001 <heavensbee001@gmail.com>
2022-08-31 17:46:47 +0200

    Replace addSkillToMember with updateMember + Add onSetSkills callback when a skill is deleted on selector component

---
+++ packages/graphql/eden/member/updateMember.ts
@@ -0,0 +1,24 @@
+import { gql } from "@apollo/client";
+
+export const UPDATE_MEMBER = gql`
+  mutation ($fields: updateMemberInput!) {
+    updateMember(fields: $fields) {
+      _id
+      discordName
+      bio
+      interest
+      timeZone
+      skills {
+        skillInfo {
+          _id
+          name
+          authors {
+            _id
+            discordName
+          }
+        }
+        level
+      }
+    }
+  }
+`;
