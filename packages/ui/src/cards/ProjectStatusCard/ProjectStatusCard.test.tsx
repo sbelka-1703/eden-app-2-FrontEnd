ee2868466ae685cb94682b8d80e06573a72782c1
Sarthak kundra <sarthakkundra21@gmail.com>
2022-09-01 22:52:43 +0530

    fixed breaking test

--- packages/ui/src/cards/ProjectStatusCard/ProjectStatusCard.test.tsx
+++ packages/ui/src/cards/ProjectStatusCard/ProjectStatusCard.test.tsx
@@ -1,11 +1,54 @@
+import { faker } from "@faker-js/faker";
 import { render } from "@testing-library/react";
+import { DateCardProps } from "../../elements/Date";
 
-import { ProjectStatusCard } from "./";
+import { OpenOrClosed, ProjectStatusCard } from "./";
+
+const kickOffDate: DateCardProps = {
+  type: "secondary",
+  dayOfMonth: 22,
+  month: "AUG",
+  year: 22,
+}
+
+const appliedDate: DateCardProps = {
+  type: "secondary",
+  dayOfMonth: 22,
+  month: "AUG",
+  year: 22,
+}
+
+const   progressSteps = [
+  {
+    name: "Applied",
+    completed: true,
+  },
+  {
+    name: "Application Reviewed",
+    completed: true,
+  },
+  {
+    name: "Application Shortlisted",
+    completed: false,
+  },
+  {
+    name: "Application Shortlisted",
+    completed: false,
+  },
+]
 
 describe("ProjectStatusCard", () => {
   it("renders without throwing", () => {
     const { container } = render(
-      <ProjectStatusCard />
+      <ProjectStatusCard
+        projectImg={faker.internet.avatar()}
+        projectName={faker.company.name()}
+        roleName="BackEnd Developer"
+        status={OpenOrClosed.OPEN}
+        appliedDateData={appliedDate}
+        kickoffDateData={kickOffDate}
+        progressSteps={progressSteps}
+      />
     );
 
     expect(container).toBeInTheDocument();
