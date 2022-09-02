fee0d562965264cbf799a666b3672d4824b72413
heavensbee001 <heavensbee001@gmail.com>
2022-09-03 01:10:43 +0200

    Add UserCardOnboardParty

--- packages/ui/src/containers/OnboardPartyContainer/OnboardPartyContainer.tsx
+++ packages/ui/src/containers/OnboardPartyContainer/OnboardPartyContainer.tsx
@@ -1,5 +1,7 @@
 import { Members } from "@graphql/eden/generated";
-import { Avatar, Badge, Card, TextHeading3 } from "ui";
+import { Card, TextHeading3 } from "ui";
+
+import { UserCardOnboardParty } from "../../cards/UserCardOnboardParty";
 
 export interface OnboardPartyContainerProps {
   members: Members[];
@@ -21,29 +23,7 @@ export const OnboardPartyContainer = ({
               (b.skills?.length || 0) - (a.skills?.length || 0)
           )
           .map((member: Members, index: number) => (
-            <Card key={index} border className="col-span-1 bg-white p-3">
-              <span
-                className={`absolute right-2 rounded-full py-1 px-2 text-xs font-medium`}
-                style={{ background: `rgba(255, 103, 103, 0.15)` }}
-              >
-                TOTAL SKILLS: {`${member.skills?.length || 0}`}
-              </span>
-
-              <div className="mb-4 flex flex-col">
-                {member.discordAvatar && (
-                  <Avatar src={member.discordAvatar} size="sm" />
-                )}
-                <span className="mt-2">{member.discordName}</span>
-              </div>
-              {member.skills?.map((skill, index) => (
-                <Badge
-                  key={index}
-                  colorRGB="209,247,196"
-                  text={skill?.skillInfo?.name || "no_name"}
-                  cutText={13}
-                />
-              ))}
-            </Card>
+            <UserCardOnboardParty key={index} member={member} />
           ))}
       </section>
     </Card>
