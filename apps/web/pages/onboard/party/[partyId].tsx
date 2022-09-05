7fa83c41450f69b96a58e368bfcf53d1676ff0d8
heavensbee001 <heavensbee001@gmail.com>
2022-09-05 14:47:27 +0200

    Fix user not entering room

--- apps/web/pages/onboard/party/[partyId].tsx
+++ apps/web/pages/onboard/party/[partyId].tsx
@@ -56,7 +56,7 @@ const OnboardPartyPage: NextPage = () => {
 
   useEffect(() => {
     // if user logged in and not in party, add currentUser to party
-    if (!partyId) return;
+    if (!currentUser || !partyId) return;
     if (
       partyId &&
       !!membersIds?.length &&
