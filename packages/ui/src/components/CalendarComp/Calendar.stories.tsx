76c2beb2bc81ab0c36b403019d52995208c6f904
ateet <ateet@propelld.com>
2022-09-08 00:20:55 +0530

    feat: calendar-comp-updated

--- packages/ui/src/components/CalendarComp/Calendar.stories.tsx
+++ packages/ui/src/components/CalendarComp/Calendar.stories.tsx
@@ -14,8 +14,9 @@ const Template: ComponentStory<typeof Calendar> = (args) => (
 
 export const Default = Template.bind({});
 Default.args = {
-  title: "Short Bio",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  isEditable: true,
+  label: "Kick Off Date",
+  timePicker: true,
+  onlyMonthPicker: false,
+  onlyYearPicker: false,
+  numberOfMonths: 1,
 };
--- packages/ui/src/components/CalendarComp/Calendar.tsx
+++ packages/ui/src/components/CalendarComp/Calendar.tsx
@@ -1,41 +1,88 @@
-import { FiEdit3 } from "react-icons/fi";
-import { Card } from "ui";
+import { useState, useRef } from "react";
+import DatePicker, { DateObject } from "react-multi-date-picker";
+import TimePicker from "react-multi-date-picker/plugins/time_picker";
+import InputIcon from "react-multi-date-picker/components/input_icon";
+import { Button } from "ui";
+import { MdEditCalendar } from "react-icons/md";
 export interface ICalendarProps {
-  title?: string;
-  description?: string;
-  onEdit?: () => void;
-  isEditable?: boolean;
+  label?: string;
+  onlyMonthPicker?: boolean;
+  onlyYearPicker?: boolean;
+  timePicker?: boolean;
+  onChange?: (data: DateObject) => void;
+  onOpen?: () => void;
+  onClose?: () => void;
+  minDate?: DateObject;
+  maxDate?: DateObject;
+  numberOfMonths?: number;
+  currentDate?: DateObject;
 }
 
 export const Calendar = ({
-  title,
-  description,
-  onEdit,
-  isEditable,
-}: ICalendarProps) => {
+  label,
+  onlyMonthPicker,
+  onlyYearPicker,
+  timePicker,
+  onChange,
+  onOpen,
+  onClose,
+  minDate,
+  maxDate,
+  numberOfMonths,
+  currentDate,
+}: ICalendarProps) =>{
+  const [inputValue, setInputValue] = useState("");
+  const datePickerRef: any = useRef();
+
+  const onSelectDate = (dateValue: DateObject) => {
+    let dateAssign = dateValue?.toDate().toLocaleDateString().toString();
+    setInputValue(dateAssign);
+    if(onChange)
+    {
+      onChange(dateValue);
+    }
+  };
+
+  const ButtonCal = () => {
+    return (
+      <Button
+        onClick={() => datePickerRef?.current?.openCalendar()}
+        radius="pill"
+      >
+        <div className="flex flex-row content-center items-center justify-between p-1">
+          <div className="mr-2">
+            <MdEditCalendar color="#BCBCBC" />
+          </div>
+          {inputValue ? (
+            <div className="font-light text-slate-600">
+              {inputValue}
+            </div>
+          ) : (
+            <div className="font-light text-slate-400">
+               {label}
+            </div>
+          )}
+        </div>
+      </Button>
+    );
+  };
   return (
-    <Card shadow>
-      <div className="flex flex-col text-sm">
-        <div
-          className={`flex flex-row content-center justify-between justify-items-stretch`}
-        >
-          <div>
-            <div className={`font-semibold`}>{title}</div>
-          </div>
-          <div className="mt-1">
-            {isEditable ? (
-              <button className={`w-full text-zinc-400`} onClick={onEdit}>
-                <FiEdit3 size="20px" />
-              </button>
-            ) : (
-              <></>
-            )}
-          </div>
-        </div>
-        <div className={`my-2 text-slate-700`}>
-          <p className="break-words text-justify">{description}</p>
-        </div>
-      </div>
-    </Card>
+    <div>
+      <DatePicker
+        onlyMonthPicker={onlyMonthPicker}
+        onlyYearPicker={onlyYearPicker}
+        className="green"
+        ref={datePickerRef}
+        render={<ButtonCal />}
+        onChange={onSelectDate}
+        minDate={minDate}
+        numberOfMonths={numberOfMonths}
+        currentDate={currentDate}
+        maxDate={maxDate}
+        onOpen={onOpen}
+        onClose={onClose}
+        plugins={timePicker ? [<TimePicker position="right" />] : []}
+      />
+    </div>
   );
 };
