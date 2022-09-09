d68b16631a399d07d89b1c9394d667fb3e658622
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2022-09-09 18:24:15 +0530

    fix: add mockedprovier to appheader for login button

--- packages/ui/src/layout/AppHeader/AppHeader.test.tsx
+++ packages/ui/src/layout/AppHeader/AppHeader.test.tsx
@@ -1,7 +1,29 @@
+import { MockedProvider } from "@apollo/client/testing";
+import { FIND_MEMBER } from "@graphql/eden";
 import { render } from "@testing-library/react";
 import { SessionProvider } from "next-auth/react";
 
 import { AppHeader } from "./";
+
+const mocks = [
+  {
+    request: {
+      query: FIND_MEMBER,
+      variables: {
+        _id: "1",
+      },
+    },
+    result: {
+      data: {
+        findMember: {
+          _id: "1",
+          discordName: "Miral",
+          emailAddress: "miralsuthar@gmail.com",
+        },
+      },
+    },
+  },
+];
 
 describe("AppHeader", () => {
   it("renders without throwing", () => {
@@ -12,7 +34,9 @@ describe("AppHeader", () => {
           user: { id: "1", email: "a", name: "Miral", image: "c" },
         }}
       >
-        <AppHeader />
+        <MockedProvider mocks={mocks}>
+          <AppHeader />
+        </MockedProvider>
       </SessionProvider>
     );
 
