a3afd4840a5775ce5a3fe1bc0ae611d7b781e2b4
sbelka-1703 <sbelka93@gmail.com>
2022-09-09 11:12:06 -0700

    Added placeholders

--- packages/ui/src/cards/UserExperienceCard/UserExperienceCard.tsx
+++ packages/ui/src/cards/UserExperienceCard/UserExperienceCard.tsx
@@ -13,16 +13,21 @@ export const UserExperienceCard = ({}: UserExperienceCardlProps) => {
   const [title, setTitle] = useState("");
   return (
     <Card shadow className="p-0">
+      <div>
+        <p>Share relevant experiences!</p>
+      </div>
       <div className="flex space-x-6">
         <div>
-          <TextField onChange={(val) => setTitle("")} />
-          <TextField onChange={(val) => setTitle("")} />
-          <TextField onChange={(val) => setTitle("")} />
+          <TextField onChange={(val) => setTitle("")} placeholder = "Position" />
+          <TextField onChange={(val) => setTitle("")} placeholder = "Company/project" />
+          <TextField onChange={(val) => setTitle("")} placeholder = "GitHub, .com" />
+          <TextField onChange={(val) => setTitle("")} placeholder = "LinkedIn, Upwork" />
           <Dropdown/>
         </div>
         <div>
           <TextArea
-          rows={7}
+          rows={9}
+          placeholder = "Type Role Desciption"
           onChange={(val) => setTitle("")} />
         </div>
         <div>
