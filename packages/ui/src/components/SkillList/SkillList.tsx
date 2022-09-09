c25ed534e33868b38969c047874b8ef33635b754
heavensbee001 <heavensbee001@gmail.com>
2022-09-09 11:23:26 +0200

    Add see more skills

--- packages/ui/src/components/SkillList/SkillList.tsx
+++ packages/ui/src/components/SkillList/SkillList.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import { Maybe, SkillType_Member } from "@graphql/eden/generated";
-import React from "react";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/outline";
+import React, { useState } from "react";
 import { Badge } from "ui";
 
 export interface SkillListProps {
@@ -14,6 +15,8 @@ export const SkillList: React.FC<SkillListProps> = ({
   colorRGB,
   handleDeleteSkill,
 }) => {
+  const [seeMore, setSeeMore] = useState(false);
+
   const badges = skills?.map(
     (skill: Maybe<SkillType_Member> | undefined, index: number) => (
       <Badge
@@ -30,5 +33,27 @@ export const SkillList: React.FC<SkillListProps> = ({
     )
   );
 
-  return <div>{badges}</div>;
+  return (
+    <div>
+      <div>
+        {badges?.slice(0, 6)}
+        {seeMore ? badges?.slice(6) : null}
+      </div>
+      {badges && badges.length > 6 && (
+        <p
+          className="cursor-pointer text-center text-sm"
+          onClick={() => setSeeMore(!seeMore)}
+        >
+          {`see ${seeMore ? "less" : "more"} skills`}
+          <span>
+            {seeMore ? (
+              <ChevronUpIcon width={16} className="ml-2 inline" />
+            ) : (
+              <ChevronDownIcon width={16} className="ml-2 inline" />
+            )}
+          </span>
+        </p>
+      )}
+    </div>
+  );
 };
