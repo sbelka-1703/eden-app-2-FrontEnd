b90a3395ba56813096a27dd19cc25a9d70935c11
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-09-11 09:12:36 -0700

    added calendars

--- packages/ui/src/cards/UserExperienceCard/UserExperienceCard.tsx
+++ packages/ui/src/cards/UserExperienceCard/UserExperienceCard.tsx
@@ -1,6 +1,7 @@
 import { Card, Dropdown, TextField } from "../../elements";
 import { useState } from "react";
 import { TextArea } from "../../elements/TextAreaComponent";
+import { Calendar } from "../../components/CalendarComp";
 export interface UserExperienceCardlProps {
   // avatar?: string;
   // title?: string;
@@ -18,20 +19,31 @@ export const UserExperienceCard = ({}: UserExperienceCardlProps) => {
       </div>
       <div className="flex space-x-6">
         <div>
-          <TextField onChange={(val) => setTitle("")} placeholder = "Position" />
-          <TextField onChange={(val) => setTitle("")} placeholder = "Company/project" />
-          <TextField onChange={(val) => setTitle("")} placeholder = "GitHub, .com" />
-          <TextField onChange={(val) => setTitle("")} placeholder = "LinkedIn, Upwork" />
-          <Dropdown/>
+          <TextField onChange={(val) => setTitle("")} placeholder="Position" />
+          <TextField
+            onChange={(val) => setTitle("")}
+            placeholder="Company/project"
+          />
+          <TextField
+            onChange={(val) => setTitle("")}
+            placeholder="GitHub, .com"
+          />
+          <TextField
+            onChange={(val) => setTitle("")}
+            placeholder="LinkedIn, Upwork"
+          />
+          <Dropdown />
         </div>
         <div>
           <TextArea
-          rows={9}
-          placeholder = "Type Role Desciption"
-          onChange={(val) => setTitle("")} />
+            rows={9}
+            placeholder="Type Role Desciption"
+            onChange={(val) => setTitle("")}
+          />
         </div>
-        <div>
-          
+        <div className="space-y-1">
+          <Calendar label="Start Date" />
+          <Calendar label="End Date" />
         </div>
       </div>
     </Card>
