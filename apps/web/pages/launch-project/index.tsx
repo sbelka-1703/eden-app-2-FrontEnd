36f7fdf6f7782eab4015653986b5152ff23e64a7
heavensbee001 <heavensbee001@gmail.com>
2022-09-11 19:52:36 +0200

    Add User profile menu + Fix some styles

---
+++ apps/web/pages/launch-project/index.tsx
@@ -0,0 +1,101 @@
+import { gql, useQuery } from "@apollo/client";
+import { LaunchProvider } from "@context/eden";
+import {
+  AppUserLayout,
+  Card,
+  GridItemSix,
+  GridItemThree,
+  GridLayout,
+  UserProfileMenu,
+} from "ui";
+
+import type { NextPageWithLayout } from "../_app";
+
+export const FIND_SERVERS = gql`
+  query ($fields: findServersInput) {
+    findServers(fields: $fields) {
+      _id
+      name
+    }
+  }
+`;
+
+// export const FIND_ROLES = gql`
+//   query ($fields: findRoleTemplatesInput) {
+//     findRoleTemplates(fields: $fields) {
+//       _id
+//       title
+//     }
+//   }
+// `;
+
+export const FIND_ROLES = gql`
+  query ($fields: findRolesInput) {
+    findRoles(fields: $fields) {
+      _id
+      description
+      name
+    }
+  }
+`;
+
+const LaunchPage: NextPageWithLayout = () => {
+  const { data: dataServers } = useQuery(FIND_SERVERS, {
+    variables: {
+      fields: {},
+    },
+    context: { serviceName: "soilservice" },
+  });
+
+  if (dataServers) console.log("dataServers", dataServers);
+
+  const { data: dataRoles } = useQuery(FIND_ROLES, {
+    variables: {
+      fields: {},
+    },
+    context: { serviceName: "soilservice" },
+  });
+
+  if (dataRoles) console.log("dataRoles", dataRoles);
+
+  return (
+    <LaunchProvider>
+      <GridLayout>
+        <GridItemThree>
+          <Card className="bg-white p-6">
+            <UserProfileMenu title={`Good Morning,`} />
+          </Card>
+        </GridItemThree>
+        <GridItemSix> </GridItemSix>
+        <GridItemThree> </GridItemThree>
+      </GridLayout>
+    </LaunchProvider>
+  );
+};
+
+LaunchPage.getLayout = (page) => <AppUserLayout>{page}</AppUserLayout>;
+
+export default LaunchPage;
+
+import { IncomingMessage, ServerResponse } from "http";
+import { getSession } from "next-auth/react";
+
+export async function getServerSideProps(ctx: {
+  req: IncomingMessage;
+  res: ServerResponse;
+}) {
+  const session = await getSession(ctx);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/login`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
