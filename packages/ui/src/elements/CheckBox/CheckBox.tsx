8f726dcd88fe24d0fd1a62f125f3ed74d798c1bb
ateet <ateet@propelld.com>
2022-09-14 01:34:57 +0530

    Feat: Checkbox added

---
+++ packages/ui/src/elements/CheckBox/CheckBox.tsx
@@ -0,0 +1,57 @@
+import clsx from "clsx";
+import { InputHTMLAttributes } from "react";
+
+export type CheckBoxProps = {
+  name?: string;
+  label?: string;
+  value?: string;
+  required?: boolean;
+  radius?: "default" | "rounded" | "pill";
+  // eslint-disable-next-line no-unused-vars
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export const CheckBox: React.FC<CheckBoxProps> = ({
+  name,
+  label,
+  value,
+  required,
+  radius = "default",
+  autoComplete,
+  placeholder,
+  onChange,
+}) => {
+  const inputCls = clsx("py-1 px-4 font-Inter text-soilBody flex", {
+    "rounded-md": radius === "default",
+    "rounded-lg": radius === "rounded",
+    "rounded-full": radius === "pill",
+  });
+
+  return (
+    <div className={`w-full`}>
+      <div className={"mt-1 flex"}>
+        <input
+          id={name}
+          name={name}
+          value={value}
+          type="checkbox"
+          required={required}
+          autoComplete={autoComplete}
+          placeholder={placeholder}
+          onChange={(e) => {
+            onChange(e);
+          }}
+          className={`${inputCls} shadow-sm`}
+        />
+        <label
+          htmlFor={name}
+          className={"text-sm font-medium text-gray-700 ml-2"}
+        >
+        {label}
+      </label>
+      </div>
+    </div>
+  );
+};
+
+export default CheckBox;
