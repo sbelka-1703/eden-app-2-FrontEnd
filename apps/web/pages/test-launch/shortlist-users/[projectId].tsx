e47d0fa1272795c9a2c612bd13437750199abad5
heavensbee001 <heavensbee001@gmail.com>
2022-09-13 20:32:58 +0200

    Add launch project flow

--- apps/web/pages/test-launch/shortlist-users/[projectId].tsx
+++ apps/web/pages/test-launch/shortlist-users/[projectId].tsx
@@ -1,13 +1,5 @@
 import { LaunchProvider } from "@context/eden";
-import {
-  AppUserLayout,
-  Card,
-  GridItemNine,
-  GridItemThree,
-  GridLayout,
-  LaunchProjectContainer,
-  UserProfileMenu,
-} from "ui";
+import { AppUserLayout, GridItemNine, GridItemThree, GridLayout } from "ui";
 
 import type { NextPageWithLayout } from "../../_app";
 
@@ -15,14 +7,8 @@ const LaunchPage: NextPageWithLayout = () => {
   return (
     <LaunchProvider>
       <GridLayout>
-        <GridItemThree>
-          <Card className="bg-white p-6">
-            <UserProfileMenu title={`Good Morning,`} />
-          </Card>
-        </GridItemThree>
-        <GridItemNine>
-          <LaunchProjectContainer />
-        </GridItemNine>
+        <GridItemThree> </GridItemThree>
+        <GridItemNine> </GridItemNine>
       </GridLayout>
     </LaunchProvider>
   );
