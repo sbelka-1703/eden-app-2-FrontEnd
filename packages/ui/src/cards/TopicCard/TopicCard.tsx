f511d5e0d89097637ab3c9844c2427ca1a1249d1
sbelka-1703 <sbelka93@gmail.com>
2022-09-22 12:17:57 -0700

    created TopicCard folder

---
+++ packages/ui/src/cards/TopicCard/TopicCard.tsx
@@ -0,0 +1,18 @@
+import { Maybe, SkillType_Member } from "@graphql/eden/generated";
+import {  Card } from "ui";
+
+export interface TopicCardProps {
+ 
+}
+export const TopicCard: TopicCardProps () => {
+    return (
+        <Card >
+         
+        </Card>
+      );
+}
+ 
+ 
+
+ 
+
