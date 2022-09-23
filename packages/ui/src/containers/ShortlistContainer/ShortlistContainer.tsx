68bbf4def7b5078c67c26e9952e5a2c9832b5c5b
heavensbee001 <heavensbee001@gmail.com>
2022-09-23 17:28:56 +0200

    Fix skills on candidates cards

--- packages/ui/src/containers/ShortlistContainer/ShortlistContainer.tsx
+++ packages/ui/src/containers/ShortlistContainer/ShortlistContainer.tsx
@@ -1,8 +1,9 @@
 import { LaunchProjectContext, LaunchProjectModal } from "@context/eden";
 import { useContext } from "react";
 
+import { TextHeading3 } from "../../atoms";
 import { AddSkillsToRoleCard, MemberMatchCard } from "../../components";
-import { Loading } from "../../elements";
+import { Card, Loading } from "../../elements";
 
 export interface IShortlistContainerProps {
   matchingMembers: any[];
@@ -32,25 +33,37 @@ export const ShortlistContainer = ({
           handleOpenSkillsModal={() => setOpenModal(LaunchProjectModal.SKILLS)}
         />
       )}
-      {selectedRole &&
-        (!matchingMembers.length ? (
-          <Loading />
-        ) : (
-          !!matchingMembers.length && (
-            <div className="grid grid-cols-3 gap-x-10 gap-y-10">
-              {matchingMembers.map((_member: any, index: number) => (
-                <MemberMatchCard
-                  key={index}
-                  onClick={() => {
-                    handleSelectMember(_member.member, _member.matchPercentage);
-                  }}
-                  member={_member.member}
-                  percentage={_member.matchPercentage}
-                />
-              ))}
-            </div>
-          )
-        ))}
+      {selectedRole && (
+        <Card className="bg-white px-10 py-4">
+          {!matchingMembers.length ? (
+            <Loading />
+          ) : (
+            !!matchingMembers.length && (
+              <>
+                <TextHeading3 className="mb-4">
+                  {selectedRole.title} matches:
+                </TextHeading3>
+                <div className="grid grid-cols-3 gap-x-10 gap-y-10">
+                  {matchingMembers.map((_member: any, index: number) => (
+                    <MemberMatchCard
+                      key={index}
+                      onClick={() => {
+                        handleSelectMember(
+                          _member.member,
+                          _member.matchPercentage
+                        );
+                      }}
+                      member={_member.member}
+                      percentage={_member.matchPercentage}
+                      requiredSkills={selectedRole.skills}
+                    />
+                  ))}
+                </div>
+              </>
+            )
+          )}
+        </Card>
+      )}
     </>
   );
 };
