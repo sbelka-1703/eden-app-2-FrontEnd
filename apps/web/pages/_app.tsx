640a1fe4eb9813bb728723af25d6fc218820d41a
e-roy <ericroupe@gmail.com>
2022-09-28 17:13:19 -0400

    feat:analyics

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -5,9 +5,12 @@ import { UserProvider } from "@context/eden";
 import { apolloClient } from "@graphql/eden";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { SessionProvider } from "next-auth/react";
 import type { ReactElement, ReactNode } from "react";
 import * as React from "react";
+
+import { IS_DEVELOPMENT, IS_PRODUCTION } from "../constants";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   // eslint-disable-next-line no-unused-vars
@@ -24,11 +27,29 @@ const App = ({
 }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page);
 
+  console.log(IS_DEVELOPMENT ? "development" : "production");
+
   return (
     <SessionProvider session={session}>
       <ApolloProvider client={apolloClient}>
         <UserProvider>{getLayout(<Component {...pageProps} />)}</UserProvider>
       </ApolloProvider>
+      {IS_PRODUCTION && (
+        <>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-6Z06JJML9S"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6Z06JJML9S');
+            `}
+          </Script>
+        </>
+      )}
     </SessionProvider>
   );
 };
