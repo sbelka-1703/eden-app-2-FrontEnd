e50943860edf87acc57c5e758ac4734d5c065ca9
ateet <ateet@propelld.com>
2022-09-28 12:32:28 +0530

    Feat: getSkill mock added

--- packages/ui/src/components/SkillVisualisationComp/SkillVisualisationComp.stories.tsx
+++ packages/ui/src/components/SkillVisualisationComp/SkillVisualisationComp.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-
+import { faker } from "@faker-js/faker";
 import { SkillVisualisationComp } from "./SkillVisualisationComp";
-
+import { getSkills } from "../../../../../apps/storybook/mocks";
 export default {
   title: "Components/SkillVisualisationComp",
   component: SkillVisualisationComp,
@@ -15,48 +15,5 @@ const Template: ComponentStory<typeof SkillVisualisationComp> = (args) => (
 export const Default = Template.bind({});
 
 Default.args = {
-  skills: [
-    {
-      skillInfo: {
-        _id: "1",
-        name: "skill1",
-      },
-    },
-    {
-      skillInfo: {
-        _id: "2",
-        name: "skill2",
-      },
-    },
-    {
-      skillInfo: {
-        _id: "3",
-        name: "skill3",
-      },
-    },
-    {
-      skillInfo: {
-        _id: "4",
-        name: "skill4",
-      },
-    },
-    {
-      skillInfo: {
-        _id: "5",
-        name: "skill5",
-      },
-    },
-    {
-      skillInfo: {
-        _id: "6",
-        name: "skill6",
-      },
-    },
-    {
-      skillInfo: {
-        _id: "7",
-        name: "skill7",
-      },
-    },
-  ],
+  skills: getSkills(faker.datatype.number({ min: 2, max: 15, precision: 1 })),
 };
