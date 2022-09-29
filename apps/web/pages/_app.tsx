b6540761692bad179aeb459a5468e8a0bea26f20
e-roy <ericroupe@gmail.com>
2022-09-28 20:54:51 -0400

    test webvitals

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -5,12 +5,16 @@ import { UserProvider } from "@context/eden";
 import { apolloClient } from "@graphql/eden";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import Script from "next/script";
 import { SessionProvider } from "next-auth/react";
 import type { ReactElement, ReactNode } from "react";
 import * as React from "react";
 
 import { IS_DEVELOPMENT, IS_PRODUCTION } from "../constants";
+import * as ga from "../lib/ga";
+
+export { reportWebVitals } from "next-axiom";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   // eslint-disable-next-line no-unused-vars
@@ -26,6 +30,24 @@ const App = ({
   pageProps: { session, ...pageProps },
 }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      ga.pageview(url);
+      console.log("url", url);
+    };
+
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   console.log(IS_DEVELOPMENT ? "development" : "production");
 
@@ -34,22 +56,19 @@ const App = ({
       <ApolloProvider client={apolloClient}>
         <UserProvider>{getLayout(<Component {...pageProps} />)}</UserProvider>
       </ApolloProvider>
-      {IS_PRODUCTION && (
-        <>
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-6Z06JJML9S"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-6Z06JJML9S"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){window.dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-6Z06JJML9S');
             `}
-          </Script>
-        </>
-      )}
+      </Script>
     </SessionProvider>
   );
 };
