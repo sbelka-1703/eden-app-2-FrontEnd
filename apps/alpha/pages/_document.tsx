75d8becb1d9d1a94b97e4e2e36c1c0de3f6277d8
e-roy <ericroupe@gmail.com>
2022-10-04 21:38:32 -0400

    fix:url cards

--- apps/alpha/pages/_document.tsx
+++ apps/alpha/pages/_document.tsx
@@ -6,8 +6,13 @@ import Document, {
   NextScript,
 } from "next/document";
 
-const appUrl = `https://eden-foundation.vercel.app/`;
-const title = `Eden protocol`;
+const appUrl =
+  process.env.NEXT_PUBLIC_ENV_BRANCH === "develop"
+    ? `https://eden-alpha-develop.vercel.app/`
+    : `https://eden-alpha.vercel.app/`;
+const title = process.env.NEXT_PUBLIC_ENV_BRANCH
+  ? `Eden protocol - alpha - ${process.env.NEXT_PUBLIC_ENV_BRANCH}`
+  : `Eden protocol - alpha`;
 const description = `Together, let's build the perfect breeding ground for everyone to do work they love. Eden's talent coordination protocol is how.`;
 
 class MyDocument extends Document {
