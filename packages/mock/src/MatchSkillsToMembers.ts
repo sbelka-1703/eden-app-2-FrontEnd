6cb07edd32396aab4e73e5f27769f7149ff7db68
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-10-06 11:52:08 -0700

    added matchPercentage

--- packages/mock/src/MatchSkillsToMembers.ts
+++ packages/mock/src/MatchSkillsToMembers.ts
@@ -1,11 +1,23 @@
 // import { PhaseType } from "@eden/package-graphql/generated";
 import { faker } from "@faker-js/faker";
 // import { phase } from "./data";
-// import { getMember } from "./MembersMock";
+import { getMember } from "./MembersMock";
 // import { getProject, randomTeam } from "./ProjectMock";
 
+export const randomPercentage = (): number => {
+  return faker.datatype.number({ min: 0, max: 100, precision: 0.00001 });
+};
+
+export const getMatchPercentage = () => ({
+  totalPercentage: randomPercentage(),
+  skillTotalPercentage: randomPercentage(),
+  hoursPercentage: randomPercentage(),
+  budgetPercentage: randomPercentage(),
+});
+
 export const matchSkillsToMembers = () => ({
-  
+  matchPercentage: getMatchPercentage(),
+  members: getMember(),
 });
 
 // export const matchMembersToProjectRole = () => ({
@@ -20,4 +32,3 @@ export const matchSkillsToMembers = () => ({
 //     author: getMember(),
 //     channelDiscordlID: faker.random.numeric(18),
 //   });
-  
