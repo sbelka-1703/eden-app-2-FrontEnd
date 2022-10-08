ae3ddd8caf2c6be11d17f1e4e8a8ecb39764f586
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-10-08 12:40:04 -0700

    Mock started

---
+++ packages/mock/src/MatchSkillsToProjects.ts
@@ -0,0 +1,25 @@
+// import { PhaseType } from "@eden/package-graphql/generated";
+import { faker } from "@faker-js/faker";
+
+import { randomPercentage } from "./MatchSkillsToMembersMock";
+
+// import { phase } from "./data";
+// import { getMember } from "./MembersMock";
+// import { getProject, randomTeam } from "./ProjectMock";
+
+export const MatchSkillsToProjects = () => ({
+  matchPercentage: randomPercentage(),
+});
+
+// export const getEpic = () => ({
+//   _id: String(faker.random.numeric(5)),
+//   name: faker.name.firstName(),
+//   desciption: faker.lorem.sentences(5),
+//   phase: faker.helpers.arrayElements(phase, 1)[0] as PhaseType,
+//   champion: getMember(),
+//   serverID: faker.random.numeric(12),
+//   project: getProject(),
+//   teams: randomTeam(),
+//   author: getMember(),
+//   channelDiscordlID: faker.random.numeric(18),
+// });
