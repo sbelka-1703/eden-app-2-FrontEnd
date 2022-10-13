fb691dc3630b384bdfc51c28382a5c792be148dc
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-10-13 08:11:25 -0700

    init

---
+++ packages/mock/src/RoleTypeMock.ts
@@ -0,0 +1,18 @@
+/* 
+
+export type RoleType = {
+__typename?: "roleType";
+_id?: Maybe<Scalars["ID"]>;
+archive?: Maybe<Scalars["Boolean"]>;
+budget?: Maybe<BudgetType>;
+dateRangeEnd?: Maybe<Scalars["String"]>;
+dateRangeStart?: Maybe<Scalars["String"]>;
+description?: Maybe<Scalars["String"]>;
+hoursPerWeek?: Maybe<Scalars["Int"]>;
+keyRosponsibilities?: Maybe<Scalars["String"]>;
+openPositions?: Maybe<Scalars["Int"]>;
+skills?: Maybe<Array<Maybe<SkillRoleType>>>;
+title?: Maybe<Scalars["String"]>;
+}; 
+
+*/
