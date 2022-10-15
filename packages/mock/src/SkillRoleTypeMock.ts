71ce739d6d6d477568326b508c5a2811c2f6ad57
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-10-14 19:56:45 -0700

    Created file

---
+++ packages/mock/src/SkillRoleTypeMock.ts
@@ -0,0 +1,20 @@
+import { faker } from "@faker-js/faker";
+
+export const SkillRoleTypeMock = () => ({
+    comment: ,
+    level: ,
+    numEndorsement: ,
+    skillData: ,
+});
+
+/* 
+
+export type SkillRoleType = {
+  __typename?: "skillRoleType";
+  comment?: Maybe<Scalars["String"]>;
+  level?: Maybe<Scalars["String"]>;
+  numEndorsement?: Maybe<Scalars["String"]>;
+  skillData?: Maybe<Skills>;
+};
+
+ */
