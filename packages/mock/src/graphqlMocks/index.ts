48e1bcd7c84aa448f9c0a25bc76e480db2682ab9
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-10-15 18:14:27 -0700

    added an index to src folder

--- packages/mock/src/graphqlMocks/index.ts
+++ packages/mock/src/graphqlMocks/index.ts
@@ -1,4 +1,6 @@
 export * from "./EpicMock";
+export * from "./findProjects_RecommendedToUser";
+export * from "./findRoleTemplates";
 export * from "./MatchSkillsToMembersMock";
 export * from "./MatchSkillsToProjects";
 export * from "./MembersMock";
---
+++ packages/mock/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./data";
+export * from "./graphqlMocks";
+export * from "./typeMocks";
