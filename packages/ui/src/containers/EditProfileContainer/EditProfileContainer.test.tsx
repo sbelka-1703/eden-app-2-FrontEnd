abf3e9061b8e3aa9dba2455dd9141843559f244a
moiz-lakkadkutta <moizlakkadkutta1@gmail.com>
2022-10-16 10:46:05 +0200

    Fixed Linting Error

--- packages/ui/src/containers/EditProfileContainer/EditProfileContainer.test.tsx
+++ packages/ui/src/containers/EditProfileContainer/EditProfileContainer.test.tsx
@@ -1,9 +1,17 @@
+import { Members } from "@eden/package-graphql/generated";
+
 import { render } from "../../../utils/jest-apollo";
 import { EditProfileContainer } from ".";
 
 describe("EditProfileContainer", () => {
   it("renders without throwing", () => {
-    const { container } = render(<EditProfileContainer />);
+    const { container } = render(
+      <EditProfileContainer
+        onSave={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    );
 
     expect(container).toBeInTheDocument();
   });
