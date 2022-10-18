f2d8e55e6fc9a65ca7241c82d01cdd3447a4afee
e-roy <ericroupe@gmail.com>
2022-10-18 08:44:27 -0400

    archived SkillSelector

--- packages/ui/src/lists/ProjectMatchList/ProjectMatchList.stories.tsx
+++ packages/ui/src/lists/ProjectMatchList/ProjectMatchList.stories.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
-import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { findProjects_RecommendedToUser } from "@eden/package-mock";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 import { ProjectMatchList } from "./ProjectMatchList";
 
