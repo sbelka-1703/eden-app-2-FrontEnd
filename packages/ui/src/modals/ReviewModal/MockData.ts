80b996fb9bca84ff0deeaaf13a8ecf1ffe11f1fa
Reza <r29taheri@gmail.com>
2022-10-26 01:17:10 +0400

    Created review modal

---
+++ packages/ui/src/modals/ReviewModal/MockData.ts
@@ -0,0 +1,64 @@
+export const mockData = {
+  main: [
+    {
+      _id: "af052ed3-fb85-4955-9d18-d2cef00acf7d",
+      name: "Frontend Developer",
+    },
+    {
+      _id: "14a62649-81b1-428d-9864-eb39127e1953",
+      name: "Design",
+    },
+  ],
+  second: [
+    {
+      _id: "f82131f7-f3ae-4010-ad98-75b24e4ae4ff",
+      name: "Other",
+    },
+    {
+      _id: "e02e8e4a-14e5-47de-8547-7b93ea42563d",
+      name: "Frontend Architecture",
+    },
+  ],
+  third: [
+    {
+      _id: "e636aaa7-9694-4a15-85d7-000e27e975ab",
+      name: "Knowledge of specific tools",
+    },
+    {
+      _id: "7dcadf77-b964-42dc-b809-9f209d437250",
+      name: "Niche Skills",
+    },
+  ],
+  "Knowledge of specific tools": [
+    {
+      _id: "57ac0b1d-4e1e-40df-97cc-d0b88c587328",
+      name: "Adobe Illustrator",
+    },
+    {
+      _id: "89fcfe81-c30c-436b-b126-0905e31128cb",
+      name: "Other",
+    },
+    {
+      _id: "9afafa56-b3d0-4394-87b1-b0d1ae5a16e3",
+      name: "Inkscape",
+    },
+    {
+      _id: "c99bd4ba-bd7c-4244-80a5-4f27e9222e04",
+      name: "Infinite Design",
+    },
+  ],
+  "Niche Skills": [
+    {
+      _id: "297a1cd0-9303-40e5-922b-c0492653b5d5",
+      name: "Design Thinking",
+    },
+    {
+      _id: "eee5da37-5fb0-490b-9e8b-1e2ae136f72a",
+      name: "Javascript",
+    },
+    {
+      _id: "cf915abf-6c3e-454a-9964-8bed68321561",
+      name: "Typescript",
+    },
+  ],
+};
---
+++ packages/ui/src/modals/ReviewModal/ReviewModal.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { mockData } from "./MockData";
+import { ReviewModal } from "./ReviewModal";
+
+export default {
+  title: "Modals/ReviewModal",
+  component: ReviewModal,
+  argTypes: {},
+} as ComponentMeta<typeof ReviewModal>;
+
+const Template: ComponentStory<typeof ReviewModal> = (args) => (
+  <ReviewModal {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  data: mockData,
+  openModal: true,
+  onClose: () => null,
+  onSubmit: () => null,
+};
