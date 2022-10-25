54f8f95e9c0788afe91b4f1658bbb4934302eb9c
e-roy <ericroupe@gmail.com>
2022-10-24 20:09:52 -0400

    update StaticCard and StaticModal

--- packages/ui/src/cards/StaticCard/StaticCard.tsx
+++ packages/ui/src/cards/StaticCard/StaticCard.tsx
@@ -10,13 +10,22 @@ import { useState } from "react";
 
 export interface IStaticCardProps {
   item?: any;
+  resultCardFlag?: any;
+  resultPopUpFlag?: any;
 }
 
-export const StaticCard = ({ item }: IStaticCardProps) => {
+export const StaticCard = ({
+  item,
+  resultCardFlag,
+  resultPopUpFlag,
+}: IStaticCardProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  // console.log(item);
+  console.log(item);
+  // console.log(resultCardFlag);
+  console.log(resultPopUpFlag);
   if (!item) return null;
+
   return (
     <Card border>
       <div className={`flex justify-between`}>
@@ -39,6 +48,29 @@ export const StaticCard = ({ item }: IStaticCardProps) => {
       <div className={`text-darkGreen font-Inter my-2 text-sm`}>
         {item?.description}
       </div>
+
+      {resultCardFlag?.type === "DAO" && <DaoFlagType item={item} />}
+      {resultCardFlag?.type === "Project" && <ProjectFlagType item={item} />}
+
+      <StaticModal
+        item={item}
+        resultPopUpFlag={resultPopUpFlag}
+        open={isOpen}
+        onClose={() => setIsOpen(!isOpen)}
+      />
+    </Card>
+  );
+};
+
+export interface IStaticCardTypeProps {
+  item?: any;
+}
+
+///////////////////////// DAO Flag Type /////////////////////////
+
+const DaoFlagType = ({ item }: IStaticCardTypeProps) => {
+  return (
+    <>
       <div className={`font-Inter text-sm text-zinc-500`}>
         🛠 Relevant Skills
       </div>
@@ -64,11 +96,38 @@ export const StaticCard = ({ item }: IStaticCardProps) => {
       <div className={`font-Inter mt-2 text-sm text-zinc-500`}>
         Eden adoptiopn in Bankless is {item?.edenMembersDAO}%
       </div>
-      <StaticModal
-        item={item}
-        open={isOpen}
-        onClose={() => setIsOpen(!isOpen)}
-      />
-    </Card>
+    </>
+  );
+};
+
+///////////////////////// Project Flag Type /////////////////////////
+
+const ProjectFlagType = ({ item }: IStaticCardTypeProps) => {
+  return (
+    <>
+      <div className={`font-Inter text-sm text-zinc-500`}>🛠 Relevant Roles</div>
+      <div>
+        {item?.roles?.map((role: any, index: number) => (
+          <Badge
+            text={role?.name}
+            key={index}
+            className={`bg-soilPurple/20 py-px text-xs`}
+          />
+        ))}
+      </div>
+      <div className={`font-Inter my-2 text-sm text-zinc-500`}>
+        👯‍♂️ Core Team
+      </div>
+      <div className="flex w-full flex-nowrap">
+        {item?.coreTeamPicture?.map((avatar: string, index: number) => (
+          <div key={index} className={`-mr-3`}>
+            <Avatar size={`xs`} src={avatar} alt={"avatar"} />
+          </div>
+        ))}
+      </div>
+      <div className={`font-Inter mt-2 text-sm text-zinc-500`}>
+        Eden adoptiopn in Bankless is {item?.edenMembersDAO}%
+      </div>
+    </>
   );
 };
