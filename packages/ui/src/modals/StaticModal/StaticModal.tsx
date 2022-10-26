22a37bbd05a41224d0abdc385e45577610460c73
heavensbee001 <heavensbee001@gmail.com>
2022-10-26 10:31:20 +0200

    Add bounty modal

--- packages/ui/src/modals/StaticModal/StaticModal.tsx
+++ packages/ui/src/modals/StaticModal/StaticModal.tsx
@@ -1,4 +1,11 @@
-import { Avatar, Badge, Button, Modal, TextHeading3 } from "@eden/package-ui";
+import {
+  Avatar,
+  Badge,
+  Button,
+  Modal,
+  TextBody,
+  TextHeading3,
+} from "@eden/package-ui";
 import { CheckCircleIcon } from "@heroicons/react/solid";
 
 export interface IStaticModalProps {
@@ -38,14 +45,32 @@ export const StaticModal = ({
         <div className={`mt-2 text-center`}>
           <TextHeading3>{item?.name}</TextHeading3>
         </div>
+        {resultPopUpFlag?.type === "Bounty" && (
+          <TextHeading3 className="text-accentColor w-full text-center">
+            ⚡️ 500 CODE ⚡️
+          </TextHeading3>
+        )}
         <div className="mb-4 flex items-start justify-between">
           <div className="flex flex-col items-start justify-center">
-            <h1 className="text-soilHeading3 font-poppins font-medium">
-              💻 about the
-            </h1>
-            <p className="text-soilBody font-Inter w-8/12 font-normal">
-              {item?.description}
-            </p>
+            {resultPopUpFlag?.type !== "Bounty" ? (
+              <>
+                <h1 className="text-soilHeading3 font-poppins font-medium">
+                  💻 about the {resultPopUpFlag?.type}
+                </h1>
+                <p className="text-soilBody font-Inter w-8/12 font-normal">
+                  {item?.description}
+                </p>
+              </>
+            ) : (
+              <>
+                <h1 className="text-soilHeading3 font-poppins font-medium">
+                  🗞 Description
+                </h1>
+                <p className="text-soilBody font-Inter w-8/12 font-normal">
+                  {item?.aboutTheBounty}
+                </p>
+              </>
+            )}
           </div>
           {item.percentage && (
             <div className="mr-12">
@@ -61,6 +86,7 @@ export const StaticModal = ({
 
         {resultPopUpFlag?.type === "DAO" && <DaoFlagType item={item} />}
         {resultPopUpFlag?.type === "Project" && <ProjectFlagType item={item} />}
+        {resultPopUpFlag?.type === "Bounty" && <BountyFlagType item={item} />}
       </div>
     </Modal>
   );
@@ -136,6 +162,73 @@ const ProjectFlagType = ({ item }: IStaticCardTypeProps) => {
       </div>
       <div className={`font-Inter mt-2 text-sm text-zinc-500`}>
         Eden adoptiopn in Bankless is {item?.edenMembersDAO}%
+      </div>
+    </>
+  );
+};
+
+///////////////////////// Bounty Flag Type /////////////////////////
+
+const BountyFlagType = ({ item }: IStaticCardTypeProps) => {
+  return (
+    <>
+      <div className="grid w-full grid-cols-4 gap-6">
+        <div className="col-span-2">
+          <p className="mb-3 text-sm font-semibold tracking-widest subpixel-antialiased">
+            🛠 Matching Skills
+          </p>
+          <div>
+            {item?.matchingSkills?.map((skill: string, index: number) => (
+              <Badge
+                text={skill}
+                key={index}
+                className={`bg-soilPurple/20 py-px text-xs`}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="col-span-2">
+          <p className="mb-3 text-sm font-semibold tracking-widest subpixel-antialiased">
+            🛠 Missing Skills
+          </p>
+          <div>
+            {item?.missingSkills?.map((skill: string, index: number) => (
+              <Badge
+                text={skill}
+                key={index}
+                className={`bg-soilPurple/20 py-px text-xs`}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="col-span-2">
+          <p className="mb-3 text-sm font-semibold tracking-widest subpixel-antialiased">
+            💻 Project
+          </p>
+          <div className="flex gap-2">
+            <Avatar size={`xs`} src={item.ProjectPicture} alt={"avatar"} />
+            <TextBody className="mt-1">{item.Project}</TextBody>
+          </div>
+        </div>
+        <div className="col-span-2">
+          <p className="mb-3 text-sm font-semibold tracking-widest subpixel-antialiased">
+            💻 about the Project
+          </p>
+          <p className="">{item.description}</p>
+        </div>
+        <div className="col-span-2">
+          <p className="mb-3 text-sm font-semibold tracking-widest subpixel-antialiased">
+            🥥 Bounty posted by
+          </p>
+          <div className="flex gap-2">
+            <Avatar
+              size={`xs`}
+              src={item.bountedPostedByPicture}
+              alt={"avatar"}
+            />
+            <TextBody className="mt-1">{item.bountedPostedBy}</TextBody>
+          </div>
+        </div>
       </div>
     </>
   );
