b0476ff358cea9985f41b0a73f36efad9728af1d
e-roy <ericroupe@gmail.com>
2022-10-26 22:10:05 -0400

    archive components

--- packages/ui/src/lists/index.ts
+++ packages/ui/src/lists/index.ts
@@ -2,7 +2,5 @@ export * from "./AvatarList";
 export * from "./ProjectChampionList";
 export * from "./ProjectList";
 export * from "./ProjectMatchList";
-export * from "./RecommendedList";
 export * from "./RoleList";
-export * from "./ShortlistList";
 export * from "./SkillList";
