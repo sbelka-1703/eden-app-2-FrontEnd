ff8bf8b323315a5e54622aef3eb276cafcc979ac
Reza <r29taheri@gmail.com>
2022-11-14 00:47:24 +0400

    Increased font size of titles

--- packages/ui/src/cards/RefineYourProfile/RefineYourProfile.tsx
+++ packages/ui/src/cards/RefineYourProfile/RefineYourProfile.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, RangeChart } from "@eden/package-ui";
+import { Button, Card, RangeChart, TextHeading2 } from "@eden/package-ui";
 
 const rangeGenerator = (min: number, range: number, max: number) => {
   const rangeNumbers: number[] = [];
@@ -13,16 +13,14 @@ export const RefineYourProfile = () => {
   return (
     <Card shadow className="mb-4 w-full bg-white p-4">
       <div>
-        <div className={` font-poppins text-xl font-medium`}>
-          Refine your profile:
-        </div>
+        <TextHeading2>Refine your profile:</TextHeading2>
         <div className="mt-8 flex justify-center">
           <Button variant="primary" onClick={() => null}>
             Add more skills
           </Button>
         </div>
         <div className="mt-8">
-          <p className="mb-4">💸 Salary Expectations</p>
+          <p className="mb-4 text-xl font-medium">💸 Salary Expectations</p>
           <RangeChart
             leftCaption="Less $"
             rightCaption="More $$"
@@ -31,8 +29,8 @@ export const RefineYourProfile = () => {
             data={rangeGenerator(0, 1000, 100)}
           />
         </div>
-        <div className="mt-8 ">
-          <p className="mb-4">⏱ Availability</p>
+        <div className="mt-8">
+          <p className="mb-4 text-xl font-medium">⏱ Availability</p>
           <RangeChart
             leftCaption=">hours"
             rightCaption="<hours"
