51502b8267fbb6c04ee198e8e9b4c63543256a7b
ateet <ateet@propelld.com>
2022-11-17 23:12:25 +0530

    feat: review Card props

--- packages/ui/src/cards/ReviewCard/ReviewCard.tsx
+++ packages/ui/src/cards/ReviewCard/ReviewCard.tsx
@@ -1,49 +1,11 @@
 import { Maybe, ProjectMemberType } from "@eden/package-graphql/generated";
-import {
-  ApplicationModal,
-  AvailabilityComp,
-  Avatar,
-  Button,
-  Card,
-  EmojiSelector,
-  GreenStepper,
-  SocialMediaComp,
-  TextBody,
-  TextHeading2,
-  TextHeading3,
-} from "@eden/package-ui";
-import { useState } from "react";
+import { Avatar, Card, EmojiSelector, TextHeading3 } from "@eden/package-ui";
 
 export interface ReviewCardProps {
   project?: Maybe<ProjectMemberType>;
 }
 
 export const ReviewCard = ({ project }: ReviewCardProps) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const STEPS_DATA = [
-    {
-      name: "engaged",
-      completed:
-        project?.phase === "engaged" ||
-        project?.phase === "shortlisted" ||
-        project?.phase === "invited",
-    },
-    {
-      name: "shortlisted",
-      completed:
-        project?.phase === "shortlisted" || project?.phase === "invited",
-    },
-    {
-      name: "invited",
-      completed: project?.phase === "invited",
-    },
-    {
-      name: "committed",
-      completed: project?.phase === "committed",
-    },
-  ];
-
   return (
     <>
       <Card shadow className={`h-60 overflow-scroll `}>
@@ -76,30 +38,7 @@ export const ReviewCard = ({ project }: ReviewCardProps) => {
           </div>
         </div>
         <div className="mb-4 px-6 text-left text-lg font-normal">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis
-          libero ex, nec finibus augue venenatis eget. Maecenas et ligula
-          ligula. Vestibulum molestie tempor justo. Maecenas lectus lacus,
-          sodales maximus sodales quis, mollis vel lacus. Nulla quam arcu,
-          interdum pellentesque erat quis, aliquam tincidunt ligula. Aliquam sed
-          nisi at metus lacinia luctus ac ut ipsum. Quisque porta leo facilisis,
-          mattis ligula vitae, aliquet risus. Nunc malesuada, ex id volutpat
-          iaculis, velit mi maximus orci, non semper massa felis sit amet magna.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis
-          libero ex, nec finibus augue venenatis eget. Maecenas et ligula
-          ligula. Vestibulum molestie tempor justo. Maecenas lectus lacus,
-          sodales maximus sodales quis, mollis vel lacus. Nulla quam arcu,
-          interdum pellentesque erat quis, aliquam tincidunt ligula. Aliquam sed
-          nisi at metus lacinia luctus ac ut ipsum. Quisque porta leo facilisis,
-          mattis ligula vitae, aliquet risus. Nunc malesuada, ex id volutpat
-          iaculis, velit mi maximus orci, non semper massa felis sit amet magna.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis
-          libero ex, nec finibus augue venenatis eget. Maecenas et ligula
-          ligula. Vestibulum molestie tempor justo. Maecenas lectus lacus,
-          sodales maximus sodales quis, mollis vel lacus. Nulla quam arcu,
-          interdum pellentesque erat quis, aliquam tincidunt ligula. Aliquam sed
-          nisi at metus lacinia luctus ac ut ipsum. Quisque porta leo facilisis,
-          mattis ligula vitae, aliquet risus. Nunc malesuada, ex id volutpat
-          iaculis, velit mi maximus orci, non semper massa felis sit amet magna.
+          {project?.role?.description}
         </div>
       </Card>
     </>
