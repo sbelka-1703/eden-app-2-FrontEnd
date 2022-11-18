abd7a548011c56cead84013d95483d766b9f9994
Reza <r29taheri@gmail.com>
2022-11-19 00:17:21 +0400

    Updated user experience card

--- packages/ui/src/cards/user/UserExperienceCard/UserExperienceCard.test.tsx
+++ packages/ui/src/cards/user/UserExperienceCard/UserExperienceCard.test.tsx
@@ -1,10 +1,20 @@
-import { render } from "@testing-library/react";
-
+import { render } from "../../../../utils/jest-apollo";
 import { UserExperienceCard } from "./";
 
 describe("UserExperienceCard", () => {
   it("renders without throwing", () => {
-    const { container } = render(<UserExperienceCard />);
+    const { container } = render(
+      <UserExperienceCard
+        roles={[]}
+        onBack={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        onNext={function (data): void {
+          console.log(data);
+          throw new Error("Function not implemented.");
+        }}
+      />
+    );
 
     expect(container).toBeInTheDocument();
   });
