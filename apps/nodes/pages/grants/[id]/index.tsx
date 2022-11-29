ac86559ca10d53ec167ec5aac0a7ceb8af38231c
e-roy <ericroupe@gmail.com>
2022-11-29 16:05:30 -0500

    fix share url

--- apps/nodes/pages/grants/[id]/index.tsx
+++ apps/nodes/pages/grants/[id]/index.tsx
@@ -1,5 +1,13 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  gql,
+  HttpLink,
+  InMemoryCache,
+  useMutation,
+} from "@apollo/client";
+import { UserContext } from "@eden/package-context";
 import { FIND_GRANTS } from "@eden/package-graphql";
+import { GrantTemplate, Mutation } from "@eden/package-graphql/generated";
 import {
   AppUserSubmenuLayout,
   Button,
@@ -8,13 +16,21 @@ import {
   GridItemEight,
   GridItemTwo,
   GridLayout,
+  Loading,
   Missing404Section,
-  // Loading,
   SEOGrants,
 } from "@eden/package-ui";
 import { useRouter } from "next/router";
 import * as React from "react";
 import { toast } from "react-toastify";
+
+export const APPLY_GRANT = gql`
+  mutation ($fields: applyGrantInput!) {
+    applyGrant(fields: $fields) {
+      _id
+    }
+  }
+`;
 
 const GrantsIdPage = ({
   grant,
@@ -24,8 +40,37 @@ const GrantsIdPage = ({
   error: string;
 }) => {
   const router = useRouter();
+  const { currentUser } = React.useContext(UserContext);
+  const [isApplying, setIsApplying] = React.useState(false);
+
+  const [applyGrant] = useMutation(APPLY_GRANT, {
+    onCompleted({ applyGrant }: Mutation) {
+      if (!applyGrant) console.log("applyGrant is null");
+      console.log("applyGrant", applyGrant);
+      // setSubmitting(false);
+      toast.success("Successfully Applied to Grant");
+      setIsApplying(false);
+    },
+  });
 
   if (error) return <Missing404Section />;
+
+  const handleApply = () => {
+    if (!currentUser) {
+      toast.error("You must be logged in to apply for a grant");
+      return;
+    }
+    setIsApplying(true);
+    applyGrant({
+      variables: {
+        fields: {
+          grantID: grant._id,
+          memberID: currentUser._id,
+        },
+      },
+    });
+  };
+
   return (
     <>
       <SEOGrants
@@ -41,27 +86,32 @@ const GrantsIdPage = ({
               shadow
               className={`h-85 scrollbar-hide overflow-y-scroll bg-white p-6`}
             >
-              <div className={`h-7/10 scrollbar-hide w-full overflow-scroll`}>
-                <GrantsInfo grant={grant} />
-              </div>
-              <div className={`flex justify-between`}>
-                <Button
-                  onClick={() => {
-                    navigator.clipboard.writeText(
-                      `${process.env.VERCEL_URL}${router.asPath}`
-                    );
-                    toast.success("grant link copied to clipboard");
-                  }}
-                >
-                  Share
-                </Button>
-                <Button
-                  variant={`primary`}
-                  onClick={() => console.log("apply")}
-                >
-                  Apply
-                </Button>
-              </div>
+              {isApplying ? (
+                <Loading title={`Applying...`} />
+              ) : (
+                <>
+                  <div
+                    className={`h-7/10 scrollbar-hide w-full overflow-scroll`}
+                  >
+                    <GrantsInfo grant={grant} />
+                  </div>
+                  <div className={`flex justify-between`}>
+                    <Button
+                      onClick={() => {
+                        navigator.clipboard.writeText(
+                          `${process.env.NEXT_PUBLIC_VERCEL_URL}${router.asPath}`
+                        );
+                        toast.success("grant link copied to clipboard");
+                      }}
+                    >
+                      Share
+                    </Button>
+                    <Button variant={`primary`} onClick={() => handleApply()}>
+                      Apply
+                    </Button>
+                  </div>
+                </>
+              )}
             </Card>
           </GridItemEight>
           <GridItemTwo> </GridItemTwo>
@@ -73,7 +123,6 @@ const GrantsIdPage = ({
 
 export default GrantsIdPage;
 
-import { GrantTemplate } from "@eden/package-graphql/generated";
 import type { GetServerSideProps } from "next";
 
 const client = new ApolloClient({
