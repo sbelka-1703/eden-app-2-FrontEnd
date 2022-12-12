9e5a78712525ab6912633d07e14c8fa5e19fb3fb
BluePanda <m.saratzidis@gmail.com>
2022-12-12 15:59:23 +0100

    feat/batteryCreateProjectPage 3

--- apps/alpha/pages/test/create-project/index.tsx
+++ apps/alpha/pages/test/create-project/index.tsx
@@ -19,6 +19,7 @@ const LAUNCH_PROJECT = gql`
   mutation ($fields: updateProjectInput!) {
     updateProject(fields: $fields) {
       _id
+      title
     }
   }
 `;
@@ -41,6 +42,8 @@ const FillProfilePage: NextPageWithLayout = () => {
   const [step, setStep] = useState(1);
 
   const [state, setState] = useState<any>({});
+
+  const [battery, setBattery] = useState(5);
 
   const [updateProject, {}] = useMutation(LAUNCH_PROJECT, {
     onCompleted({ updateProject }: Mutation) {
@@ -132,11 +135,20 @@ const FillProfilePage: NextPageWithLayout = () => {
   const stepView = () => {
     switch (step) {
       case 1:
-        return <CreateProjectViews1 data={state[1]} onNext={onNext} />;
+        return (
+          <CreateProjectViews1
+            battery={battery}
+            setBattery={setBattery}
+            data={state[1]}
+            onNext={onNext}
+          />
+        );
 
       case 2:
         return (
           <CreateProjectViews2
+            battery={battery}
+            setBattery={setBattery}
             onNext={onNext}
             projects={typeProjectNodes?.findNodes}
             onBack={() => setStep((prev) => prev - 1)}
@@ -145,6 +157,8 @@ const FillProfilePage: NextPageWithLayout = () => {
       case 3:
         return (
           <CreateProjectViews7
+            battery={battery}
+            setBattery={setBattery}
             onNext={onNext}
             expertise={expertiseNodes?.findNodes}
             onBack={() => setStep((prev) => prev - 1)}
@@ -153,9 +167,9 @@ const FillProfilePage: NextPageWithLayout = () => {
       case 4:
         return (
           <CreateProjectViews6
-            onNext={() => null}
+            onNext={onClickLaunch}
             onLaunch={onClickLaunch}
-            onNewPosition={() => null}
+            onNewPosition={() => setStep((prev) => prev - 1)}
             onBack={() => setStep((prev) => prev - 1)}
           />
         );
