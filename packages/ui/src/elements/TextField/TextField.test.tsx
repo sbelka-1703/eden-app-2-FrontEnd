be9b9f47f48a1a355e0a43dc9b494008e956120e
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-12-15 08:01:14 -0700

    TextField, first test started

--- packages/ui/src/elements/TextField/TextField.test.tsx
+++ packages/ui/src/elements/TextField/TextField.test.tsx
@@ -1,13 +1,17 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { TextField } from "./";
 
-describe("TextField", () => {
-  it("renders without throwing", () => {
-    const { container } = render(
-      <TextField onChange={(val) => console.log(val)} />
-    );
+test("When a user is typing, it trigers an onChange event", async () => {
+  const user = userEvent.setup();
+  const onChange = jest.fn();
 
-    expect(container).toBeInTheDocument();
-  });
+  render(<TextField onChange={onChange} />);
+  const inputTextBox = screen.getByRole("textbox");
+
+  await user.type(inputTextBox, "Hello, World");
+  expect(onChange).toHaveBeenCalledTimes(12);
 });
+
+test.todo("When the TextField is disabled, the user cannot type in it");
