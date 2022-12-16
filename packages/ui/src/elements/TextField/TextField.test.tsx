aabc17b801b84f5b0d5b0a6a7d2e361caa877919
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-12-16 07:49:32 -0700

    TextField test updated

--- packages/ui/src/elements/TextField/TextField.test.tsx
+++ packages/ui/src/elements/TextField/TextField.test.tsx
@@ -3,15 +3,25 @@ import userEvent from "@testing-library/user-event";
 
 import { TextField } from "./";
 
-test("When a user is typing, it trigers an onChange event", async () => {
+test("When a user is typing in an enabled TextFeild, it trigers an onChange event", async () => {
   const user = userEvent.setup();
   const onChange = jest.fn();
 
-  render(<TextField onChange={onChange} />);
+  const { rerender } = render(
+    <TextField onChange={onChange} label={"label"} disabled={false} />
+  );
+
+  //Testing the label props
+  expect(screen.getByText("label")).toBeInTheDocument();
   const inputTextBox = screen.getByRole("textbox");
 
   await user.type(inputTextBox, "Hello, World");
+  //It takes 12 keystrokes to type "Hello, World"
+  expect(onChange).toHaveBeenCalledTimes(12);
+
+  rerender(<TextField onChange={onChange} disabled={true} />);
+
+  await user.type(inputTextBox, "Hello, World");
+  //If the the textField is disabled user should not trigger an onChange event
   expect(onChange).toHaveBeenCalledTimes(12);
 });
-
-test.todo("When the TextField is disabled, the user cannot type in it");
