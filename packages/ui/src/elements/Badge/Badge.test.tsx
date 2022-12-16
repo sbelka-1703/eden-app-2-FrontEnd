31b972c6e1b4c69d1c5184201a7f8331e207f3a0
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-12-16 12:45:01 -0700

    Todo tests created for Badge

--- packages/ui/src/elements/Badge/Badge.test.tsx
+++ packages/ui/src/elements/Badge/Badge.test.tsx
@@ -2,16 +2,8 @@ import { render } from "@testing-library/react";
 
 import { Badge } from "./";
 
-describe("Badge", () => {
-  it("renders without throwing", () => {
-    const { container } = render(
-      <Badge
-        colorRGB="236, 240, 71, 40"
-        text="Design"
-        onClose={() => console.log("")}
-      />
-    );
+test.todo("When a user presses the X, it triggers the onClose event ");
 
-    expect(container).toBeInTheDocument();
-  });
-});
+test.todo(
+  "When the prop for cut is set to Two, the user can only see two letters"
+);
