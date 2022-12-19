e790d88e413b384072b150ee92ab65b95d6151b2
e-roy <ericroupe@gmail.com>
2022-12-19 17:44:46 -0500

    removed unused SignUpContext

--- packages/context/src/index.ts
+++ packages/context/src/index.ts
@@ -3,5 +3,4 @@ export * from "./GrantsContext";
 export * from "./HackathonContext";
 export * from "./LaunchProjectContext";
 export * from "./ProjectsContext";
-export * from "./SignUpContext";
 export * from "./UserContext";
