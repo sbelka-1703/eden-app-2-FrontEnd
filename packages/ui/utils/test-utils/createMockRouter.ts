e0b85b38bd873b2b925bf008df6124c82e83e946
Sbelka <79952005+sbelka-1703@users.noreply.github.com>
2022-12-23 13:16:05 -0700

    Update packages/ui/utils/test-utils/createMockRouter.ts
    Co-authored-by: with-heart <with.heart+git@pm.me>

--- packages/ui/utils/test-utils/createMockRouter.ts
+++ packages/ui/utils/test-utils/createMockRouter.ts
@@ -1,29 +1,33 @@
 import { NextRouter } from "next/router";
 
+const defaultRouter: NextRouter = {
+  basePath: "",
+  pathname: "/",
+  route: "/",
+  query: {},
+  asPath: "/",
+  back: jest.fn(),
+  beforePopState: jest.fn(),
+  prefetch: jest.fn(),
+  push: jest.fn(),
+  reload: jest.fn(),
+  replace: jest.fn(),
+  events: {
+    on: jest.fn(),
+    off: jest.fn(),
+    emit: jest.fn(),
+  },
+  isFallback: false,
+  isLocaleDomain: false,
+  isReady: true,
+  defaultLocale: "en",
+  domainLocales: [],
+  isPreview: false,
+};
+
 export function createMockRouter(router: Partial<NextRouter>): NextRouter {
   return {
-    basePath: "",
-    pathname: "/",
-    route: "/",
-    query: {},
-    asPath: "/",
-    back: jest.fn(),
-    beforePopState: jest.fn(),
-    prefetch: jest.fn(),
-    push: jest.fn(),
-    reload: jest.fn(),
-    replace: jest.fn(),
-    events: {
-      on: jest.fn(),
-      off: jest.fn(),
-      emit: jest.fn(),
-    },
-    isFallback: false,
-    isLocaleDomain: false,
-    isReady: true,
-    defaultLocale: "en",
-    domainLocales: [],
-    isPreview: false,
+    ...defaultRouter,
     ...router,
-  } as NextRouter;
+  };
 }
