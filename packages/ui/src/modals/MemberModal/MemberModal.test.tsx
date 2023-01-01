a8679682791012c04b2a392e0f62d8512c596fc0
e-roy <ericroupe@gmail.com>
2023-01-01 10:03:00 -0500

    add mock provider to test components

--- packages/ui/src/modals/MemberModal/MemberModal.test.tsx
+++ packages/ui/src/modals/MemberModal/MemberModal.test.tsx
@@ -1,10 +1,15 @@
+import { MockedProvider } from "@apollo/client/testing";
 import { render } from "@testing-library/react";
 
 import { MemberModal } from "./MemberModal";
 
 describe("MemberModal", () => {
   it("renders without throwing", () => {
-    const { container } = render(<MemberModal />);
+    const { container } = render(
+      <MockedProvider>
+        <MemberModal />
+      </MockedProvider>
+    );
 
     expect(container).toBeInTheDocument();
   });
--- packages/ui/src/modals/UserInviteModal/UserInviteModal.test.tsx
+++ packages/ui/src/modals/UserInviteModal/UserInviteModal.test.tsx
@@ -1,3 +1,4 @@
+import { MockedProvider } from "@apollo/client/testing";
 import { getMember } from "@eden/package-mock";
 
 // import { render } from "@testing-library/react";
@@ -6,7 +7,11 @@ import { UserInviteModal } from "./UserInviteModal";
 
 describe("UserInviteModal", () => {
   it("renders without throwing", () => {
-    const { container } = render(<UserInviteModal member={getMember()} />);
+    const { container } = render(
+      <MockedProvider>
+        <UserInviteModal member={getMember()} />
+      </MockedProvider>
+    );
 
     expect(container).toBeInTheDocument();
   });
