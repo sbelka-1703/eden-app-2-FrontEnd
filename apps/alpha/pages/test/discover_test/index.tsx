9aba110d488fe9d5b762ec71c166a367e8a291c3
OleanjiKingCode <holamilean56@gmail.com>
2023-01-12 20:20:34 +0100

    Modal Intro View

--- apps/alpha/pages/test/discover_test/index.tsx
+++ apps/alpha/pages/test/discover_test/index.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@apollo/client";
 import {
   DiscoverContext,
+  DiscoverModal,
   DiscoverProvider,
   UserContext,
 } from "@eden/package-context";
@@ -61,12 +62,12 @@ const DiscoverPage: NextPageWithLayout = () => {
       <GridLayout>
         <GridItemFour>
           <div className="flex flex-col gap-4 ">
-            <Card
-              className={`scrollbar-hide h-85 overflow-scroll bg-white p-4 `}
-            >
-              <span className="text-lg font-semibold">
-                Find out your Profile
-              </span>
+            <Card className={`scrollbar-hide overflow-scroll bg-white p-4 `}>
+              {openModal !== DiscoverModal.SKILLS_CATEGORY && (
+                <span className="text-lg font-semibold">
+                  Who are you looking for?
+                </span>
+              )}
               <DiscoverContainer
                 setArrayOfNodes={(val) => {
                   setNodesID(val);
