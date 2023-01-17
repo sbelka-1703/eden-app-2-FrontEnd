2256a624038518cbb790902c276cb4066d89f528
e-roy <ericroupe@gmail.com>
2023-01-17 11:21:39 -0500

    fix on create project

--- apps/alpha/pages/create-project/index.tsx
+++ apps/alpha/pages/create-project/index.tsx
@@ -53,3 +53,28 @@ const CreateProjectPage: NextPageWithLayout = () => {
 CreateProjectPage.getLayout = (page) => <AppUserLayout>{page}</AppUserLayout>;
 
 export default CreateProjectPage;
+
+import { IncomingMessage, ServerResponse } from "http";
+import { getSession } from "next-auth/react";
+
+export async function getServerSideProps(ctx: {
+  req: IncomingMessage;
+  res: ServerResponse;
+}) {
+  const session = await getSession(ctx);
+
+  const url = ctx.req.url?.replace("/", "");
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/login?redirect=${url}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
