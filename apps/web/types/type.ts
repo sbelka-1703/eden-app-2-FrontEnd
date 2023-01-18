6a42bf95caddf708224910a57c8f6e58e5e6e4bc
e-roy <ericroupe@gmail.com>
2023-01-17 20:04:41 -0500

    fix type error?

--- apps/web/types/type.ts
+++ apps/web/types/type.ts
@@ -34,7 +34,7 @@ export interface CreateMessageApiRequestBody {
   senderName?: string;
   senderAvatarURL?: string;
   channelId?: string;
-  tagName: string;
+  tagName?: string;
   threadName?: string;
   ThreadAutoArchiveDuration?: ThreadAutoArchiveDuration;
 }
