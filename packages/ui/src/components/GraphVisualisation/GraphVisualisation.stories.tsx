9b818e5f36e0c203a81627451bba6a43c8cbf29e
BluePanda <m.saratzidis@gmail.com>
2023-01-21 18:51:56 +0100

    feat/G6_graphVisualtiotionMVP

--- packages/ui/src/components/GraphVisualisation/GraphVisualisation.stories.tsx
+++ packages/ui/src/components/GraphVisualisation/GraphVisualisation.stories.tsx
@@ -16,4 +16,29 @@ const Template: ComponentStory<typeof GraphVisualisation> = (args) => {
 export const Default = Template.bind({});
 Default.args = {
   member: getMember(),
+  data2: {
+    nodes: [
+      { id: "node0", size: 50 },
+      { id: "node1", size: 30 },
+      { id: "node2", size: 30 },
+      { id: "node3", size: 30 },
+      { id: "node4", size: 30 },
+      { id: "node5", size: 30 },
+      { id: "node6", size: 15 },
+      { id: "node7", size: 15 },
+      { id: "node8", size: 15 },
+      { id: "node9", size: 15 },
+    ],
+    edges: [
+      { source: "node0", target: "node1" },
+      { source: "node0", target: "node2" },
+      { source: "node0", target: "node3" },
+      { source: "node0", target: "node4" },
+      { source: "node0", target: "node5" },
+      { source: "node1", target: "node6" },
+      { source: "node1", target: "node7" },
+      { source: "node2", target: "node8" },
+      { source: "node2", target: "node9" },
+    ],
+  },
 };
--- packages/ui/src/components/GraphVisualisation/GraphVisualisation.tsx
+++ packages/ui/src/components/GraphVisualisation/GraphVisualisation.tsx
@@ -6,40 +6,46 @@ import React, { useEffect, useState } from "react";
 
 export interface IGraphVisualisation {
   member: Members;
+  data2: any;
 }
 
-const data = {
-  nodes: [
-    { id: "node0", size: 50 },
-    { id: "node1", size: 30 },
-    { id: "node2", size: 30 },
-    { id: "node3", size: 30 },
-    { id: "node4", size: 30 },
-    { id: "node5", size: 30 },
-    { id: "node6", size: 15 },
-    { id: "node7", size: 15 },
-    { id: "node8", size: 15 },
-    { id: "node9", size: 15 },
-  ],
-  edges: [
-    { source: "node0", target: "node1" },
-    { source: "node0", target: "node2" },
-    { source: "node0", target: "node3" },
-    { source: "node0", target: "node4" },
-    { source: "node0", target: "node5" },
-    { source: "node1", target: "node6" },
-    { source: "node1", target: "node7" },
-    { source: "node2", target: "node8" },
-    { source: "node2", target: "node9" },
-  ],
-};
+// const data = {
+//   nodes: [
+//     { id: "node0", size: 50 },
+//     { id: "node1", size: 30 },
+//     { id: "node2", size: 30 },
+//     { id: "node3", size: 30 },
+//     { id: "node4", size: 30 },
+//     { id: "node5", size: 30 },
+//     { id: "node6", size: 15 },
+//     { id: "node7", size: 15 },
+//     { id: "node8", size: 15 },
+//     { id: "node9", size: 15 },
+//   ],
+//   edges: [
+//     { source: "node0", target: "node1" },
+//     { source: "node0", target: "node2" },
+//     { source: "node0", target: "node3" },
+//     { source: "node0", target: "node4" },
+//     { source: "node0", target: "node5" },
+//     { source: "node1", target: "node6" },
+//     { source: "node1", target: "node7" },
+//     { source: "node2", target: "node8" },
+//     { source: "node2", target: "node9" },
+//   ],
+// };
 
-export const GraphVisualisation = ({ member }: IGraphVisualisation) => {
+const data = {};
+
+export const GraphVisualisation = ({ member, data2 }: IGraphVisualisation) => {
+  // export const GraphVisualisation = ({ member, data }: IGraphVisualisation) => {
   const ref = React.useRef(null);
   let graph: any = null;
 
+  console.log("data2 = ", data2);
+
   useEffect(() => {
-    if (!graph) {
+    if (!graph && data2) {
       // Minimap
       const minimap = new G6.Minimap();
 
@@ -98,7 +104,7 @@ export const GraphVisualisation = ({ member }: IGraphVisualisation) => {
         refreshDragedNodePosition(e);
       });
     }
-  }, []);
+  }, [data2]);
 
   return (
     <>
