a19a83f6205e568790c5054c08869a15aad24d15
e-roy <ericroupe@gmail.com>
2023-01-22 09:04:37 -0500

    add missing module

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,7 @@ const withTM = require("next-transpile-modules")([
   "@eden/package-ui",
   "@eden/package-context",
   "@eden/package-graphql",
+  "@eden/package-mock",
 ]);
 
 module.exports = withTM({
