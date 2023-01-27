e1980b8a92c4732ef6cf96741c9fca4dd2f179f1
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-01-27 13:53:20 -0700

    init

---
+++ packages/ui/src/charts/SalaryRangeChart_1/SalaryRangeChart.tsx
@@ -0,0 +1,30 @@
+import { RangeChart, TextHeading3 } from "@eden/package-ui";
+
+export interface SalaryRangeChart_1Props {
+  data: number[];
+  // eslint-disable-next-line no-unused-vars
+  onChange?: (data: {
+    domain: number[];
+    update: number[];
+    values: number[];
+  }) => void;
+}
+
+export const SalaryRangeChart = ({
+  data,
+  onChange,
+}: SalaryRangeChart_1Props) => {
+  return (
+    <div>
+      <TextHeading3 className="mb-3 text-center">
+        Hourly salary range
+      </TextHeading3>
+      <RangeChart
+        data={data}
+        onChange={onChange}
+        minCaption="min salary"
+        maxCaption="max salary"
+      />
+    </div>
+  );
+};
--- packages/ui/src/charts/index.ts
+++ packages/ui/src/charts/index.ts
@@ -1,5 +1,6 @@
 export * from "./Charts";
 export * from "./RangeChart";
 export * from "./SalaryRangeChart";
+export * from "./SalaryRangeChart_1";
 export * from "./TeamAttributeChart";
 export * from "./UserAttributeChart";
