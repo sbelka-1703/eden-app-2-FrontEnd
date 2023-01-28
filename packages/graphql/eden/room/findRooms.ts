6d2a2b10b4ed4ec2a95dd4ff162c0036e283feb2
e-roy <ericroupe@gmail.com>
2023-01-28 16:26:52 -0500

    pagination for findRooms query

--- packages/graphql/eden/room/findRooms.ts
+++ packages/graphql/eden/room/findRooms.ts
@@ -3,22 +3,27 @@ import { gql } from "@apollo/client";
 export const FIND_ROOMS = gql`
   query ($fields: findRoomsInput) {
     findRooms(fields: $fields) {
-      _id
-      name
-      description
-      avatar
-      members {
+      roomsData {
         _id
-        discordName
-        discordAvatar
+        name
+        description
+        avatar
+        members {
+          _id
+          discordName
+          discordAvatar
+        }
+        hosts {
+          _id
+          discordName
+          discordAvatar
+        }
+        serverID
+        registeredAt
       }
-      hosts {
-        _id
-        discordName
-        discordAvatar
+      pageInfo {
+        totalResults
       }
-      serverID
-      registeredAt
     }
   }
 `;
