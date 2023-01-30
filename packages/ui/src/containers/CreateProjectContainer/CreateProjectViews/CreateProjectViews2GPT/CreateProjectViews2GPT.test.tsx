d418456acb46531dcb14191d0e5a50a401311ae6
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-01-30 11:11:47 -0700

    fixed testing errors

--- packages/ui/src/containers/CreateProjectContainer/CreateProjectViews/CreateProjectViews2GPT/CreateProjectViews2GPT.test.tsx
+++ packages/ui/src/containers/CreateProjectContainer/CreateProjectViews/CreateProjectViews2GPT/CreateProjectViews2GPT.test.tsx
@@ -1,22 +1,46 @@
+import { MockedProvider } from "@apollo/client/testing";
 import { render } from "@testing-library/react";
 
 import { CreateProjectViews2GPT } from ".";
+// const mocks = [
+//   {
+//     request: {
+//       query: MESSAGE_TO_GPT,
+//       variables: {
+//         fields: {
+//           message: "example text",
+//           category: "skill",
+//           prompt: "this is",
+//         },
+//       },
+//     },
+//     result: {
+//       data: {
+//         messageToGPT: {
+//           message: "example response",
+//         },
+//       },
+//     },
+//   },
+// ];
 
 describe("CreateProjectViews2GPT", () => {
   it("renders without throwing", () => {
     const { container } = render(
-      <CreateProjectViews2GPT
-        onBack={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-        battery={2}
-        onNext={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-        setProject={function (data): void {
-          console.info({ data });
-        }}
-      />
+      <MockedProvider>
+        <CreateProjectViews2GPT
+          onBack={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          battery={2}
+          onNext={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          setProject={function (data): void {
+            console.info({ data });
+          }}
+        />
+      </MockedProvider>
     );
 
     expect(container).toBeInTheDocument();
