a3be46b087393d83d6c19c2898d9a36f2ebfd462
e-roy <ericroupe@gmail.com>
2023-01-31 13:02:06 -0500

    fix test failure

--- packages/ui/src/containers/index.ts
+++ packages/ui/src/containers/index.ts
@@ -7,7 +7,5 @@ export * from "./FillUserProfileContainer";
 export * from "./GrantsModalContainer";
 export * from "./NodesOnboardPartyContainer";
 export * from "./ProjectsModalContainer";
-export * from "./test/HackathonModalContainer";
-export * from "./test/ShortlistModalContainerStoryFilter";
 export * from "./ViewProjectContainer";
 export * from "./ViewUserProfileContainer";
