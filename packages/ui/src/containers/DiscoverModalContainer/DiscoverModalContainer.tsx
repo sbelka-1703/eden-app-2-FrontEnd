6105c715b46579fed9c565e91ca44dbee395ebde
miralsuthar <miralsuthar2000@gmail.com>
2023-02-16 22:35:19 +0530

    Fix: discover DiscoverModalContainer

--- packages/ui/src/containers/DiscoverModalContainer/DiscoverModalContainer.tsx
+++ packages/ui/src/containers/DiscoverModalContainer/DiscoverModalContainer.tsx
@@ -1,7 +1,7 @@
 import { DiscoverContext, DiscoverModal } from "@eden/package-context";
 import { PreferencesType } from "@eden/package-graphql/generated";
 import {
-  DiscoverTalentDropdownWithSkillSelectorModal,
+  DiscoverTalentDropdownModal,
   PreferencesModal,
   PrioritizeModal,
   RequirementsModal,
@@ -88,7 +88,7 @@ export const DiscoverModalContainer = ({
       )}
 
       {openModal === DiscoverModal.SKILLS_CATEGORY && (
-        <DiscoverTalentDropdownWithSkillSelectorModal
+        <DiscoverTalentDropdownModal
           openModal={openModal === DiscoverModal.SKILLS_CATEGORY}
           onClose={() => {
             setOpenModal(DiscoverModal.SKIP_ALERT);
@@ -110,7 +110,7 @@ export const DiscoverModalContainer = ({
       )}
 
       {openModal === DiscoverModal.SKILLS_SUBCATEGORY && (
-        <DiscoverTalentDropdownWithSkillSelectorModal
+        <DiscoverTalentDropdownModal
           openModal={openModal === DiscoverModal.SKILLS_SUBCATEGORY}
           onClose={() => {
             setOpenModal(DiscoverModal.SKIP_ALERT);
