afceb3f7c2e5065c411b8057d97fe197aaf27180
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-03-20 10:43:13 -0700

    getting a string back to the FE

--- apps/alpha/pages/test/cv_gpt/index.tsx
+++ apps/alpha/pages/test/cv_gpt/index.tsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
 interface CvGPTProps {}
 
 const CvGPT: CvGPTProps = () => {
-  return <div>HI!</div>;
+  const [file, setFile] = useState<File | null>(null);
+  const [string, setString] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | null) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    const response = await fetch("../api/extract-pdf/extract-pdf", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (response.ok) {
+      console.log("response", response);
+      const data = await response.json();
+
+      // setString(data);
+
+      console.log("data", data);
+
+      console.log("data.text", data.text);
+    } else {
+      console.error("Error uploading and extracting file.");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={handleChange}></input>
+      <button className="border-2 border-red-400" type="submit">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default CvGPT;
