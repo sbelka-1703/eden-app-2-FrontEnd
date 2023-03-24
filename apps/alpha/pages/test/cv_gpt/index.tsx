8c6e8321484507bea2d81b5350b8d7195c17086b
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-03-24 11:18:47 -0700

    visuals for upload complete and the FE compoentent is ready

--- apps/alpha/pages/test/cv_gpt/index.tsx
+++ apps/alpha/pages/test/cv_gpt/index.tsx
@@ -1,7 +1,67 @@
-interface CvGPTProps {}
+import { ChangeEvent, useState } from "react";
 
-const CvGPT: CvGPTProps = () => {
-  return <div></div>;
+const CvGPT = () => {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!file) {
+      return;
+    }
+    const reader = new FileReader();
+
+    reader.onloadend = async () => {
+      const base64File = ((reader.result as string) || "").split(",")[1];
+      const response = await fetch("../api/process-pdf/process-pdf", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ fileBuffer: base64File }),
+      });
+
+      if (response.ok) {
+        const { text } = await response.json;
+
+        console.log(text);
+      } else {
+        const { error } = await response.json;
+
+        console.log(error);
+      }
+
+      reader.readAsDataURL(file);
+    };
+  };
+
+  return (
+    <div className="w-72 ">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center justify-center space-y-2"
+      >
+        <label>Resume(CV)</label>
+        <label htmlFor="input" className="text-center text-sm">
+          Upload Recent Resume for Better Results Form Our AI
+        </label>
+        <input
+          className="ml-60"
+          onChange={handleFileChange}
+          type="file"
+        ></input>
+        <button
+          className="border-2 border-blue-400 hover:bg-blue-700 hover:text-white rounded-lg hover:border-blue-700 text-blue-400 font-bold px-2"
+          type="submit"
+        >
+          Upload Resume
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CvGPT;
