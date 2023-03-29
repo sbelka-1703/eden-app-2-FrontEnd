b37e2f27ef59e34dcf64b23d4b3bd36f0e887853
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-03-29 13:27:42 -0700

    created resizeObserverMock to overcome the ResizeObserver error

--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "jest";
 
 const config: Config = {
   preset: "@eden/package-jest-presets/jest/jsdom",
+  setupFilesAfterEnv: ["../mock/src/utils/resizeObserverMock.ts"],
 };
 
 export default config;
