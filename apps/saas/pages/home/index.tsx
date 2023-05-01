a643b8acfec153e6cdc8dc4bff243c84fdd4bed9
heavensbee <heavensbee001@gmail.com>
2023-05-01 23:43:25 +0200

    Put saas pages into roject

--- apps/saas/pages/home/index.tsx
+++ apps/saas/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { AppUserSubmenuLayout, HomeHeroSection, SEO } from "@eden/package-ui";
+import { AppUserLayout, SEO } from "@eden/package-ui";
 
 import type { NextPageWithLayout } from "../_app";
 
@@ -6,18 +6,39 @@ const HomePage: NextPageWithLayout = () => {
   return (
     <>
       <SEO />
-      <HomeHeroSection projectsPage usersPage launchPage />
+      <div className="w-full p-8">
+        <h2>Quick directory</h2>
+        <ul className="list-disc">
+          <li>
+            <Link href={"/companyCRM/644e052ca7177f51e7c27b77"}>
+              Company CRM
+            </Link>
+          </li>
+          <li>
+            <Link href={"/companyCRM/TrainQuestionsEdenAI"}>
+              Company CRM Train questions
+            </Link>
+          </li>
+          <li>
+            <Link href={"/interviewEdenAIpage"}>Interview</Link>
+          </li>
+          <li>
+            <Link href={"/interviewEdenAIpage/644e052ca7177f51e7c27b77"}>
+              Interview Company
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
 
-HomePage.getLayout = (page) => (
-  <AppUserSubmenuLayout>{page}</AppUserSubmenuLayout>
-);
+HomePage.getLayout = (page) => <AppUserLayout>{page}</AppUserLayout>;
 
 export default HomePage;
 
 import { IncomingMessage, ServerResponse } from "http";
+import Link from "next/link";
 import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(ctx: {
