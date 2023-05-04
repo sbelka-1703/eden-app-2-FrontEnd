c8625d35681cc3a9c1a80657bede0316e6610232
heavensbee <heavensbee001@gmail.com>
2023-05-04 14:57:27 +0200

    Fix most of Type missalignments

--- apps/saas/pages/dashboard/[companyID].tsx
+++ apps/saas/pages/dashboard/[companyID].tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { CandidateType } from "@eden/package-graphql/generated/graphqlEden";
 import { AppUserLayout, Button, CandidatesTableList } from "@eden/package-ui";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
@@ -36,6 +37,15 @@ const FIND_COMPANY = gql`
             positionName
           }
         }
+        readyToDisplay
+        summaryQuestions {
+          questionID
+          questionContent
+          answerContent
+          reason
+          score
+          bestAnswerCompany
+        }
       }
       questionsToAsk {
         bestAnswer
@@ -59,16 +69,16 @@ export interface Data {
 export interface FindCompany {
   _id: string;
   name: string;
-  candidates: Candidate[];
+  candidates: CandidateType[];
   questionsToAsk: QuestionsToAsk[];
   __typename: string;
 }
 
-export interface Candidate {
-  overallScore: number | null;
-  user: IUser;
-  __typename: string;
-}
+// export interface Candidate {
+//   overallScore: number | null;
+//   user: IUser;
+//   __typename: string;
+// }
 
 export interface IUser {
   _id: string;
@@ -180,17 +190,19 @@ type QuestionType = {
 //   // Add more users as needed
 // ];
 
-type CandidateType = {
-  _id: number;
-  name: string;
-  avatar: string;
-  score: number;
-  role?: string;
-  background?: any[];
-  level?: string;
-  usdcHour?: number;
-  responseRate?: number;
-};
+// type CandidateType = {
+//   _id: number;
+//   name: string;
+//   avatar: string;
+//   score: number;
+//   role?: string;
+//   background?: any[];
+//   level?: string;
+//   usdcHour?: number;
+//   responseRate?: number;
+//   reason: string;
+//   summaryQuestions: summaryQuestionType[];
+// };
 // type Users = User[];
 
 // const users: Users = [
@@ -238,24 +250,27 @@ const CompanyCRM: NextPageWithLayout = () => {
     skip: !Boolean(companyID), // DEV: Simplified the comparisson to be boolean . before was: companyID == "" || companyID == null,  (note the "==" at least is on purpose)
     ssr: false,
     onCompleted: (data: any) => {
-      // console.log("createRoom completed", data);
-      setCandidates(
-        data.findCompany.candidates.map((candidate: Candidate) => {
-          return {
-            _id: parseInt(candidate.user._id),
-            name: candidate.user.discordName,
-            avatar: candidate.user.discordAvatar,
-            score: candidate.overallScore, //
-            usdcHour: candidate.user.budget.perHour,
-            background: candidate.user.previousProjects?.map(
-              (project: any) => project.title
-            ),
-            role: candidate.user.memberRole?.title,
-            level: "Junior", // candidate.user...,
-            responseRate: 15, // candidate.user.chat....
-          };
-        })
-      );
+      setCandidates(data.findCompany.candidates);
+
+      // setCandidates(
+      //   data.findCompany.candidates.map((candidate: CandidateType) => {
+      //     return {
+      //       _id: parseInt(candidate.user!._id!),
+      //       name: candidate.user!.discordName,
+      //       avatar: candidate.user!.discordAvatar,
+      //       score: candidate.overallScore, //
+      //       usdcHour: candidate.user!.budget!.perHour,
+      //       background: candidate.user!.previousProjects?.map(
+      //         (project: any) => project.title
+      //       ),
+      //       role: candidate.user!.memberRole?.title,
+      //       level: "Junior", // candidate.user...,
+      //       responseRate: 15, // candidate.user.chat....
+      //       // reason: candidate.summaryQuestions,
+      //       summaryQuestions: candidate.summaryQuestions,
+      //     };
+      //   })
+      // );
 
       const questionPrep: QuestionType[] = [];
 
@@ -279,12 +294,6 @@ const CompanyCRM: NextPageWithLayout = () => {
       setQuestions(questionPrep);
     },
   });
-
-  // console.log("dataCompany = ", dataCompany);
-
-  console.log("companyID = ", companyID);
-
-  console.log("questions = ", questions);
 
   const [selectedUser, setSelectedUser] = useState<CandidateType | null>(null);
   const [trainModalOpen, setTrainModalOpen] = useState(false);
@@ -346,53 +355,10 @@ const CompanyCRM: NextPageWithLayout = () => {
         Train AI
       </Button>
       {selectedUser && (
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-screen items-end justify-center px-4 pb-20 pt-4 text-center sm:block sm:p-0">
-            <div
-              className="fixed inset-0 transition-opacity"
-              aria-hidden="true"
-              onClick={() => setSelectedUser(null)}
-            >
-              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-            </div>
-            <span
-              className="hidden sm:inline-block sm:h-screen sm:align-middle"
-              aria-hidden="true"
-            ></span>
-            <div className="inline-block transform overflow-hidden rounded-lg bg-white text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle">
-              <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src={selectedUser.avatar}
-                      alt={selectedUser.name}
-                    />
-                  </div>
-                  <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                    <h3 className="text-lg    font-medium leading-6 text-gray-900">
-                      {selectedUser.name}
-                    </h3>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Score: {selectedUser.score}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                <button
-                  type="button"
-                  className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => setSelectedUser(null)}
-                >
-                  Close
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <SelectedUser
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
       )}
       {trainModalOpen && (
         <div className="fixed inset-0 z-10 overflow-y-auto">
@@ -427,3 +393,112 @@ const CompanyCRM: NextPageWithLayout = () => {
 CompanyCRM.getLayout = (page: any) => <AppUserLayout>{page}</AppUserLayout>;
 
 export default CompanyCRM;
+
+// type summaryQuestionType = {
+//   questionID: number;
+//   questionContent: string;
+//   answerContent: string;
+//   bestAnswerCompany: string;
+//   // reason: string;
+//   score: number;
+// };
+
+type Props = {
+  selectedUser: CandidateType;
+  setSelectedUser: React.Dispatch<React.SetStateAction<CandidateType | null>>;
+};
+
+const SelectedUser: React.FC<Props> = ({ selectedUser, setSelectedUser }) => {
+  return (
+    <div className="fixed inset-0 z-10 overflow-y-auto">
+      <div className="flex min-h-screen items-end justify-center px-4 pb-20 pt-4 text-center sm:block sm:p-0">
+        <div
+          className="fixed inset-0 transition-opacity"
+          aria-hidden="true"
+          onClick={() => {
+            setSelectedUser(null);
+          }}
+        >
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+        <span
+          className="hidden sm:inline-block sm:h-screen sm:align-middle"
+          aria-hidden="true"
+        ></span>
+        <div className="inline-block transform overflow-hidden rounded-lg bg-white text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle">
+          <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-100 sm:mx-0 sm:h-10 sm:w-10">
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src={selectedUser?.user?.discordAvatar!}
+                  alt={selectedUser?.user?.discordName!}
+                />
+              </div>
+              <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                <h3 className="mb-4 text-2xl font-medium leading-6 text-black">
+                  {selectedUser?.user?.discordName}
+                </h3>
+                <div className="mt-2">
+                  <p className="inline-block rounded-lg px-4 py-2 text-4xl font-bold text-indigo-600 shadow-lg">
+                    {selectedUser?.overallScore}
+                    <span className="tooltip">
+                      <svg
+                        className="h-6 w-6 text-gray-400 hover:text-gray-600"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M12 19V12M12 8V8M12 4L12.01 4M20 4H4C2.89543 4 2 4.89543 2 6V18C2 19.1046 2.89543 20 4 20H20C21.1046 20 22 19.1046 22 18V6C22 4.89543 21.1046 4 20 4Z"
+                        ></path>
+                      </svg>
+                      {/* <span className="tooltiptext">
+                        {selectedUser?.reason}
+                      </span> */}
+                    </span>
+                  </p>
+                  {selectedUser?.summaryQuestions &&
+                    selectedUser?.summaryQuestions?.map((question, index) => (
+                      <div key={index}>
+                        <p className="mb-2 mt-4 text-2xl font-medium leading-6 text-black">
+                          {question?.questionContent}
+                        </p>
+                        <p className="mb-2 text-sm text-gray-500">
+                          Answer: {question?.answerContent}
+                        </p>
+                        {question?.score && (
+                          <p className="inline-block rounded-lg px-4 py-2 text-lg font-bold text-indigo-400 shadow-xl">
+                            Score: {question?.score}
+                          </p>
+                        )}
+                        {/* {question.reason && (
+                          <p className="text-sm text-gray-500">
+                            Reason: {question.reason}
+                          </p>
+                        )} */}
+                      </div>
+                    ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="px4 bg-gray-50 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+            <button
+              type="button"
+              className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+              onClick={() => {
+                setSelectedUser(null);
+              }}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
