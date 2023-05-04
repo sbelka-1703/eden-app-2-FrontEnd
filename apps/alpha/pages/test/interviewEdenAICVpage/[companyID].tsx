6e6d8822a780d4f203228e01c21b20f554a940b3
BluePanda <m.saratzidis@gmail.com>
2023-05-04 09:28:40 +0200

    add it also on the companyID page

--- apps/alpha/pages/test/interviewEdenAICVpage/[companyID].tsx
+++ apps/alpha/pages/test/interviewEdenAICVpage/[companyID].tsx
@@ -2,11 +2,27 @@ import { gql, useMutation, useQuery } from "@apollo/client";
 import { UserContext } from "@eden/package-context";
 import {
   AI_INTERVIEW_SERVICES,
+  Button,
   ChatMessage,
   InterviewEdenAI,
 } from "@eden/package-ui";
 import { useRouter } from "next/router";
-import React, { useContext, useEffect, useState } from "react";
+import React, {
+  ChangeEvent,
+  FormEvent,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+export const CV_TO_MEMORY = gql`
+  mutation ($fields: storeLongTermMemorySummaryInput) {
+    storeLongTermMemorySummary(fields: $fields) {
+      success
+      message
+    }
+  }
+`;
 
 const FIND_COMPANY = gql`
   query ($fields: findCompanyInput) {
@@ -137,6 +153,64 @@ const InterviewEdenAIpage: React.FC = () => {
 
   console.log("chatN = ", chatN);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [file, setFile] = useState<File | null>(null);
+
+  const [CVtoMemory] = useMutation(CV_TO_MEMORY, {
+    onCompleted({ CVtoMemory }) {
+      console.log("CVtoMemory.success==== ", CVtoMemory);
+      setIsLoading(false);
+    },
+  });
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!file) {
+      return;
+    }
+
+    setIsLoading(true);
+    const reader = new FileReader();
+
+    reader.onloadend = async () => {
+      const base64File = ((reader.result as string) || "").split(",")[1];
+      const response = await fetch("../api/process-pdf/process-pdf", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ fileBuffer: base64File }),
+      });
+
+      if (response.ok) {
+        const { text } = await response.json();
+
+        CVtoMemory({
+          variables: { fields: { message: text, userID: currentUser?._id } },
+        });
+
+        // CVtoJobs({ variables: { fields: { cvString: text } } });
+
+        // CVtoNodes({ variables: { fields: { message: text } } });
+
+        console.log(text);
+      } else {
+        const { error } = await response.json();
+
+        console.log("error aaa", error);
+      }
+    };
+
+    reader.readAsDataURL(file);
+  };
+
   return (
     <div className="flex h-screen w-full">
       <div className="flex h-full w-1/2 flex-col space-y-4 p-4">
@@ -145,6 +219,25 @@ const InterviewEdenAIpage: React.FC = () => {
         </h1>
       </div>
       <div className="flex h-screen flex-col items-center justify-center text-lg">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center justify-center space-y-2"
+        >
+          <label>Resume(CV)</label>
+          <label htmlFor="input" className="text-center text-sm">
+            Upload Recent Resume Here
+          </label>
+          <input
+            className="ml-60"
+            onChange={handleFileChange}
+            type="file"
+            accept=".pdf"
+          ></input>
+
+          <Button loading={isLoading} type="submit" variant="primary">
+            {isLoading ? "Uploading Resume..." : "Upload Resume"}
+          </Button>
+        </form>
         <div className="h-[60vh]">
           {
             <InterviewEdenAI
