0c4436daaeb3696ba25737d70615f1ad19125982
heavensbee <heavensbee001@gmail.com>
2023-05-10 19:53:31 +0200

    Make upload cv mutation automatically

--- packages/ui/src/components/CVUploadGPT/CVUploadGPT.tsx
+++ packages/ui/src/components/CVUploadGPT/CVUploadGPT.tsx
@@ -1,7 +1,15 @@
 import { gql, useMutation } from "@apollo/client";
 import { UserContext } from "@eden/package-context";
-import { ChangeEvent, FormEvent, useContext, useState } from "react";
+import {
+  ChangeEvent,
+  // FormEvent,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { toast } from "react-toastify";
 
+import { Loading } from "../../elements";
 // export const CV_TO_SUMMARY = gql`
 //   mutation ($fields: CVtoSummaryInput!) {
 //     CVtoSummary(fields: $fields) {
@@ -26,6 +34,7 @@ export interface ICVUploadGPTProps {
 // eslint-disable-next-line no-unused-vars
 export const CVUploadGPT = ({ timePerWeek, seed }: ICVUploadGPTProps) => {
   const [file, setFile] = useState<File | null>(null);
+  const [uploading, setUploading] = useState<boolean>(false);
   // const [summary, setSummary] = useState<string | null>(null);
   const { currentUser } = useContext(UserContext);
 
@@ -35,6 +44,16 @@ export const CVUploadGPT = ({ timePerWeek, seed }: ICVUploadGPTProps) => {
     //   console.log("SaveCVtoUser.result", SaveCVtoUser.result);
     //   setSummary(SaveCVtoUser.result);
     // },
+    onCompleted: (data) => {
+      console.log("------>", data);
+
+      setUploading(false);
+      toast.success("success");
+    },
+    onError: (err) => {
+      setUploading(false);
+      toast.error(err.message);
+    },
   });
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -43,46 +62,86 @@ export const CVUploadGPT = ({ timePerWeek, seed }: ICVUploadGPTProps) => {
     }
   };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!file) {
-      return;
-    }
-    const reader = new FileReader();
+  useEffect(() => {
+    if (file) {
+      setUploading(true);
+      const reader = new FileReader();
 
-    reader.onloadend = async () => {
-      const base64File = ((reader.result as string) || "").split(",")[1];
-      const response = await fetch("../api/process-pdf/process-pdf", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ fileBuffer: base64File }),
-      });
-
-      if (response.ok) {
-        const { text } = await response.json();
-
-        SaveCVtoUser({
-          variables: {
-            fields: {
-              // cvString: text
-              userID: currentUser?._id,
-              cvContent: text,
-            },
+      reader.onloadend = async () => {
+        const base64File = ((reader.result as string) || "").split(",")[1];
+        const response = await fetch("../api/process-pdf/process-pdf", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
           },
+          body: JSON.stringify({ fileBuffer: base64File }),
         });
 
-        console.log(text);
-      } else {
-        const { error } = await response.json();
+        if (response.ok) {
+          const { text } = await response.json();
 
-        console.log("error aaa", error);
-      }
-    };
+          SaveCVtoUser({
+            variables: {
+              fields: {
+                // cvString: text
+                userID: currentUser?._id,
+                cvContent: text,
+              },
+            },
+          });
 
-    reader.readAsDataURL(file);
-  };
+          console.log(text);
+        } else {
+          const { error } = await response.json();
+
+          console.log("error aaa", error);
+        }
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }, [file]);
+
+  // const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   if (!file) {
+  //     return;
+  //   }
+  //   const reader = new FileReader();
+
+  //   reader.onloadend = async () => {
+  //     const base64File = ((reader.result as string) || "").split(",")[1];
+  //     const response = await fetch("../api/process-pdf/process-pdf", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ fileBuffer: base64File }),
+  //     });
+
+  //     if (response.ok) {
+  //       const { text } = await response.json();
+
+  //       SaveCVtoUser({
+  //         variables: {
+  //           fields: {
+  //             // cvString: text
+  //             userID: currentUser?._id,
+  //             cvContent: text,
+  //           },
+  //         },
+  //       });
+
+  //       console.log(text);
+  //     } else {
+  //       const { error } = await response.json();
+
+  //       console.log("error aaa", error);
+  //     }
+  //   };
+
+  //   reader.readAsDataURL(file);
+  // };
 
   // const summaryList = summary
   //   ? summary
@@ -96,7 +155,7 @@ export const CVUploadGPT = ({ timePerWeek, seed }: ICVUploadGPTProps) => {
   return (
     <div className="w-fit ">
       <form
-        onSubmit={handleSubmit}
+        // onSubmit={handleSubmit}
         className="flex flex-col items-center justify-center space-y-2"
       >
         {/* <label>Resume(CV)</label> */}
@@ -109,12 +168,13 @@ export const CVUploadGPT = ({ timePerWeek, seed }: ICVUploadGPTProps) => {
           type="file"
           accept=".pdf"
         ></input>
-        <button
+        {uploading && <Loading title="uploading" />}
+        {/* <button
           className="rounded-lg border-2 border-blue-400 px-2 font-bold text-blue-400 hover:border-blue-700 hover:bg-blue-700 hover:text-white"
           type="submit"
         >
           Upload Resume
-        </button>
+        </button> */}
       </form>
       {/* {summary ? (
         <div className="ml-2 mt-2 w-fit rounded-md border-2 border-black pl-6 pr-4 ">
